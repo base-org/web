cbff059d2a7beb3f410698ed098cb313f1b32011
Matthew Bunday <matthew.bunday@coinbase.com>
2023-09-27 09:12:21 -0400

    Link to ecosystem bridges in OFAC modal

--- apps/bridge/src/components/OFACModal/OFACModal.tsx
+++ apps/bridge/src/components/OFACModal/OFACModal.tsx
@@ -1,23 +1,8 @@
 import { Modal } from 'apps/bridge/src/components/Modal/Modal';
-import Image from 'next/image';
 
 type OFACModalProps = {
   isOpen: boolean;
 };
-
-function BridgeCard({ name, url, imageFilename, domain }: { name: string; url: string; imageFilename: string, domain: string }) {
-  return (
-    <a href={url}>
-      <div className="flex flex-row gap-4 bg-neutral-800 p-4 rounded-8">
-        <Image src={`/images/partners/${imageFilename}`} alt={name} width={48} height={48} className="rounded-full" />
-        <div className="flex flex-col">
-          <p className="text-white text-md text-left">{name}</p>
-          <p className="text-sm font-mono">{domain}</p>
-        </div>
-      </div>
-    </a>
-  );
-}
 
 export function OFACModal({ isOpen }: OFACModalProps) {
   return (
@@ -26,11 +11,7 @@ export function OFACModal({ isOpen }: OFACModalProps) {
       title="THIS SERVICE IS NOT AVAILABLE IN YOUR REGION"
       content={(
         <div>
-          <p className="text-white mt-4">Alternatively, you can use these approved third-party bridges.</p>
-          <div className="flex flex-row gap-8 mt-8">
-            <BridgeCard name="Hop" url="https://app.hop.exchange" imageFilename="hop.png" domain="app.hop.exchange" />
-            <BridgeCard name="Bungee" url="https://bungee.exchange" imageFilename="bungee.png" domain="bungee.exchange" />
-          </div>
+          <p className="text-white mt-4">Alternatively, you can use these <a href="https://base.org/ecosystem?tag=bridge" className="underline">approved third-party bridges</a>.</p>
         </div>
   )}
     />
