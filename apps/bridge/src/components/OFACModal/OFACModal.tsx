735abb8c805dc2a9aa95c0b8f56934a4bf09a1f4
Matthew Bunday <matthew.bunday@coinbase.com>
2023-10-25 08:46:53 -0400

    Allow successfully initiated withdrawals to complete

--- apps/bridge/src/components/OFACModal/OFACModal.tsx
+++ apps/bridge/src/components/OFACModal/OFACModal.tsx
@@ -1,4 +1,5 @@
 import { Modal } from 'apps/bridge/src/components/Modal/Modal';
+import Link from 'next/link';
 
 type OFACModalProps = {
   isOpen: boolean;
@@ -9,11 +10,22 @@ export function OFACModal({ isOpen }: OFACModalProps) {
     <Modal
       isOpen={isOpen}
       title="THIS SERVICE IS NOT AVAILABLE IN YOUR REGION"
-      content={(
-        <div>
-          <p className="text-white mt-4">Alternatively, you can use these <a href="https://base.org/ecosystem?tag=bridge" className="underline">approved third-party bridges</a>.</p>
+      content={
+        <div className="mt-4 text-white">
+          <p>
+            <Link href="/transactions" className="underline">
+              View your existing transaction history.
+            </Link>
+          </p>
+          <p>
+            Alternatively, you can use these{' '}
+            <a href="https://base.org/ecosystem?tag=bridge" className="underline">
+              approved third-party bridges
+            </a>
+            .
+          </p>
         </div>
-  )}
+      }
     />
   );
 }
