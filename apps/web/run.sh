7e5c33e93ef2f2c5a061b0f3e0efff5080825f98
angel-ding-cb <angel.ding@coinbase.com>
2023-11-16 15:40:42 -0600

    move all secret env to config service

---
+++ apps/web/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# check if envmapper is loaded
+if [ -f /envmapper/mapping.env ];  then
+    echo "Loading envmapper"
+    source /envmapper/mapping.env
+fi
+env
+exec next start
