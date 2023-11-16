17c7491046eb94272dfbab4071e9bb6041c67732
angel-ding-cb <angel.ding@coinbase.com>
2023-11-16 13:11:41 -0600

    fixed typo

--- apps/base-docs/run.sh
+++ apps/base-docs/run.sh
@@ -2,7 +2,7 @@
 set -e
 
 # check if envmapper is loaded
-if [ -f /envmapper/mapper.env ];  then
+if [ -f /envmapper/mapping.env ];  then
     echo "Loading envmapper"
     source /envmapper/mapping.env
 fi
