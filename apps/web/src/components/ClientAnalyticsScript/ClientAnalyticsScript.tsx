240885d6844a6af33fb8121cd38385cfa94757cf
Jacob Moore <jacob.moore@coinbase.com>
2024-01-17 16:10:57 -0800

    Ignore eslint rule affecting useCookie from @coinbase/cookie-manager.

--- apps/web/src/components/ClientAnalyticsScript/ClientAnalyticsScript.tsx
+++ apps/web/src/components/ClientAnalyticsScript/ClientAnalyticsScript.tsx
@@ -7,12 +7,14 @@ import initCCA from '../../utils/initCCA';
 export default function ClientAnalyticsScript() {
   const router = useRouter();
   const trackingPreference = useSavedTrackingPreference();
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const [deviceIdCookie, setDeviceIdCookie] = useCookie('base_device_id');
 
   return (
     <Script
       src="https://static-assets.coinbase.com/js/cca/v0.0.1.js"
       onLoad={useCallback(
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
         () => initCCA(router, trackingPreference, deviceIdCookie, setDeviceIdCookie),
         [router, trackingPreference, deviceIdCookie, setDeviceIdCookie],
       )}
