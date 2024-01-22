b549065dddcbeb3649889f3bef00c385e4d62183
jacob-moore-cb <jacob.moore@coinbase.com>
2024-01-22 09:56:56 -0800

    Add Cookie Manager and Add CCA Device Tracking (#257)
    * Add Coinbase cookie banner to Base Web.
    
    * Base Web - Add cookie policy page and cookie manager. Init CCA with device ID cookie.
    
    * Base Docs - Add cookie policy page and cookie manager. Init CCA with device ID cookie.
    
    * Add custom CookieBanner theme to improve contrast.
    
    * Ignore eslint rule affecting useCookie from @coinbase/cookie-manager.

--- apps/base-docs/src/theme/Root.jsx
+++ apps/base-docs/src/theme/Root.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable */
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import {
   connectorsForWallets,
   getDefaultWallets,
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
-import { useEffect, useState } from 'react';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import { baseGoerli, baseSepolia } from 'wagmi/chains';
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+import { Provider as CookieManagerProvider, Region } from '@coinbase/cookie-manager';
+import { cookieManagerConfig } from '../utils/cookieManagerConfig';
+import { CookieBanner } from '@coinbase/cookie-banner';
 
 export const { chains, publicClient } = configureChains(
   [baseGoerli, baseSepolia],
@@ -32,12 +35,101 @@ const wagmiConfig = createConfig({
   publicClient,
 });
 
+const cookieBannerTheme = {
+  colors: {
+    primary: '#1652F0',
+    positive: '#05B169',
+    negative: '#DF5F67',
+    warning: '#F4C622',
+    background: '#FFFFFF',
+    backgroundMuted: '#EEF0F3',
+    onBackground: '#050F1A',
+    onBackgroundMuted: '#0A0B0D',
+    onPrimary: '#FFFFFF',
+    overlay: 'rgba(17,52,83,0.6)',
+  },
+  border: {
+    border: '1px solid #D8D8D8',
+    borderRadius: '4px',
+  },
+  fontSize: {
+    sm: '14px',
+    md: '16px',
+  },
+  fontWeight: {
+    regular: '400',
+    bold: '500',
+  },
+  size: {
+    xs: '8px',
+    sm: '16px',
+    md: '24px',
+    lg: '32px',
+  },
+  breakpoints: {
+    phone: 560,
+    desktop: 992,
+    tablet: 768,
+  },
+  zIndex: {
+    high: 2,
+    overlay: 1000,
+  },
+};
+
 export default function Root({ children }) {
   const [mounted, setMounted] = useState(false);
+
+  // Cookie Consent Manager Provider Configuration
+  const trackingPreference = useRef();
+
+  const setTrackingPreference = useCallback((newPreference) => {
+    const priorConsent = trackingPreference.current?.consent;
+    trackingPreference.current = newPreference;
+
+    if (!priorConsent) {
+      // The first time the modal appears, this function is called with nothing present in
+      // trackingPreference.current. To avoid an infinite refresh loop, we return early on
+      // the first call.
+      return;
+    }
+
+    const newConsent = newPreference.consent;
+
+    // Check if the preferences have changed.
+    const diff = [
+      ...priorConsent.filter((elem) => !newConsent.includes(elem)),
+      ...newConsent.filter((elem) => !priorConsent.includes(elem)),
+    ];
+
+    // Reload if the preferences have changed.
+    if (diff.length > 0) {
+      window.location.reload();
+    }
+  }, []);
+
+  const handleLogError = useCallback((err) => console.error(err), []);
+
   useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
   return (
     <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>{children}</RainbowKitProvider>
+      <RainbowKitProvider chains={chains}>
+        <CookieManagerProvider
+          projectName="base_docs"
+          locale="en"
+          region={Region.DEFAULT}
+          log={console.log}
+          onError={handleLogError}
+          onPreferenceChange={setTrackingPreference}
+          config={cookieManagerConfig}
+        >
+          {children}
+          <CookieBanner companyName="Base" link="/cookie-policy" theme={cookieBannerTheme} />
+        </CookieManagerProvider>
+      </RainbowKitProvider>
     </WagmiConfig>
   );
 }
