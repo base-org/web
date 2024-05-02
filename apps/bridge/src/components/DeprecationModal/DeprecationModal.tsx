dd97ec49d763aa546ecfa6b4e59c9b8c4b40f168
Matthew Bunday <matthew.bunday@coinbase.com>
2024-05-02 10:01:18 -0700

    Add deprecation modal

---
+++ apps/bridge/src/components/DeprecationModal/DeprecationModal.tsx
@@ -0,0 +1,41 @@
+import { useCallback, useMemo } from 'react';
+import { Modal } from 'apps/bridge/src/components/Modal/Modal';
+import { useLocalStorage } from 'usehooks-ts';
+
+export function DeprecationModal() {
+  const [deprecationSeen, setDeprecationSeen] = useLocalStorage('deprecationSeen', false);
+
+  const onClose = useCallback(() => {
+    setDeprecationSeen(true);
+  }, [setDeprecationSeen]);
+
+  const content = useMemo(() => {
+    return (
+      <div className="flex flex-col items-center space-y-8">
+        <p>
+          Starting next week, users will be redirected to the Superchain Bridges. See FAQs for
+          details.
+        </p>
+        <div className="flex flex-col space-y-2">
+          <button
+            type="button"
+            className="rounded bg-white px-8 py-3.5 font-sans text-black"
+            onClick={onClose}
+          >
+            Continue
+          </button>
+        </div>
+      </div>
+    );
+  }, []);
+
+  return (
+    <Modal
+      isOpen={!deprecationSeen}
+      title="THIS BRIDGE IS BEING REPLACED SOON"
+      icon="deprecation"
+      content={content}
+      onClose={onClose}
+    />
+  );
+}
