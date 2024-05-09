519b74fcda3cc0115fdf827a67922fc131901b42
Matthew Bunday <matthew.bunday@coinbase.com>
2024-05-09 15:11:52 -0400

    Mock out first screen

--- apps/web/src/components/Usernames/Search/index.tsx
+++ apps/web/src/components/Usernames/Search/index.tsx
@@ -1,4 +1,6 @@
 import SearchIcon from 'apps/web/src/components/Icon/SearchIcon';
+import Image from 'next/image';
+import UsernameFlair from 'apps/web/public/images/usernames/UsernameFlair.png';
 
 type UsernameResult = {
   name: string;
@@ -50,25 +52,30 @@ function UsernameResultRow({ name, available, tooShort }: UsernameResult) {
 export default function Search() {
   const usernames = useUsernames();
   return (
-    <div className="flex w-1/2 flex-col gap-6 text-white">
-      <h1 className="text-3xl uppercase">YOUR BASE USERNAME</h1>
-      <p>Search for and create a .base.eth username</p>
-      <div className="flex flex-col gap-2">
-        <div className="relative flex w-full flex-row items-center">
-          <input
-            type="text"
-            placeholder="search for a name"
-            className="w-full rounded border border-solid border-muted bg-darkgray px-3 py-4 text-white focus:border-2 focus:border-white focus:outline-none "
-          />
-          <button className="absolute right-[10px]" type="submit">
-            <SearchIcon />
-          </button>
+    <div className="flex w-full flex-row justify-around px-12">
+      <div className="flex w-1/2 max-w-[600px] flex-col gap-6 text-white">
+        <h1 className="text-3xl uppercase">YOUR BASE USERNAME</h1>
+        <p>Search for and create a .base.eth username</p>
+        <div className="flex flex-col gap-2">
+          <div className="relative flex w-full flex-row items-center">
+            <input
+              type="text"
+              placeholder="search for a name"
+              className="w-full rounded border border-solid border-muted bg-darkgray px-3 py-4 text-white focus:border-2 focus:border-white focus:outline-none "
+            />
+            <button className="absolute right-[10px]" type="submit">
+              <SearchIcon />
+            </button>
+          </div>
+          <div>
+            {usernames.map((data) => (
+              <UsernameResultRow key={data.name} {...data} />
+            ))}
+          </div>
         </div>
-        <div>
-          {usernames.map((data) => (
-            <UsernameResultRow key={data.name} {...data} />
-          ))}
-        </div>
+      </div>
+      <div className="flex w-1/2 flex-col items-end">
+        <Image src={UsernameFlair} alt="base circles in a square arrangement" />
       </div>
     </div>
   );
