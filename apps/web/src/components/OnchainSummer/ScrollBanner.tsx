8e395ad0520ca00bdbd7e0ea0c86994d79d79b9b
Matthew Bunday <matthew.bunday@coinbase.com>
2024-05-14 12:01:40 -0400

    get scroll banner working

--- apps/web/src/components/OnchainSummer/ScrollBanner.tsx
+++ apps/web/src/components/OnchainSummer/ScrollBanner.tsx
@@ -3,25 +3,79 @@ function Circle() {
   return <div className="mx-4 h-4 w-4 rounded-full border-2 border-white" />;
 }
 
+import { motion } from 'framer-motion';
+
+const textVariants = {
+  animate: {
+    x: [0, -window.innerWidth], // animate from 0 to negative window width
+    transition: {
+      x: {
+        repeat: Infinity,
+        repeatType: 'loop',
+        duration: 10,
+        ease: 'linear',
+      },
+    },
+  },
+};
+
+function ScrollingText({ children }: { children: React.ReactNode }) {
+  return (
+    <motion.div
+      className="inline-block flex flex-row items-center whitespace-nowrap"
+      variants={textVariants}
+      animate="animate"
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+function ContentA() {
+  return (
+    <>
+      <Circle />
+      onchain summer is back
+    </>
+  );
+}
+
+function ContentB() {
+  return (
+    <>
+      <Circle />
+      it's time to build
+    </>
+  );
+}
+
 export default function ScrollBanner() {
   return (
-    <div className="my-8 flex flex-row text-2xl uppercase text-white">
-      <div className="flex flex-row items-center">
-        <Circle />
-        It's time to buikd
-      </div>
-      <div className="flex flex-row items-center">
-        <Circle />
-        onchain summer is back
-      </div>
-      <div className="flex flex-row items-center">
-        <Circle />
-        It's time to buikd
-      </div>
-      <div className="flex flex-row items-center">
-        <Circle />
-        onchain summer is back
-      </div>
+    <div className="my-8 flex w-full overflow-hidden whitespace-nowrap text-2xl uppercase text-white">
+      <ScrollingText>
+        <ContentA />
+        <ContentB />
+        <ContentA />
+        <ContentB />
+        <ContentA />
+        <ContentB />
+        <ContentA />
+        <ContentB />
+        <ContentA />
+        <ContentB />
+        <ContentA />
+        <ContentB />
+        <ContentA />
+        <ContentB />
+        <ContentA />
+        <ContentB />
+        <ContentA />
+        <ContentB />
+        <ContentA />
+        <ContentB />
+        <ContentA />
+        <ContentB />
+      </ScrollingText>
     </div>
   );
 }
