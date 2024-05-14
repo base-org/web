f9deca1a32a39458f6e569ff27f172707e672dd5
Matthew Bunday <matthew.bunday@coinbase.com>
2024-05-14 16:34:14 -0400

    join us onchain random leter

--- apps/web/src/components/OnchainSummer/Circles.tsx
+++ apps/web/src/components/OnchainSummer/Circles.tsx
@@ -1,11 +1,11 @@
 // white circle
 export function WhiteCircle() {
-  return <div className="h-4 w-4 rounded-full border-2 border-white bg-white" />;
+  return <div className="h-[.8rem] w-[.8rem] rounded-full border-2 border-white bg-white" />;
 }
 
 // black css circle
 export function BlackCircle() {
-  return <div className="h-4 w-4 rounded-full border-2 border-black bg-black" />;
+  return <div className="h-[.8rem] w-[.8rem] rounded-full border-2 border-black bg-black" />;
 }
 
 // empty black css circle
--- apps/web/src/components/OnchainSummer/EventsBlock.tsx
+++ apps/web/src/components/OnchainSummer/EventsBlock.tsx
@@ -1,3 +1,17 @@
+import Brit from 'apps/web/src/components/OnchainSummer/Brit';
+import { BlackCircle } from 'apps/web/src/components/OnchainSummer/Circles';
+
 export default function EventsBlock() {
-  return <div className="flex flex-row">EventsBlock</div>;
+  return (
+    <div className="my-12 flex flex-col">
+      <div className="text-l flex flex-row items-center gap-2 px-8 font-mono uppercase">
+        <BlackCircle /> Events
+      </div>
+      <div className="my-6 flex flex-col md:flex-row md:items-end">
+        <span className="px-8 text-5xl font-extrabold leading-9 md:text-7xl">
+          J<Brit axis={147}>o</Brit>IN <Brit axis={183}>u</Brit>S ON<Brit axis={60}>c</Brit>HAIN
+        </span>
+      </div>
+    </div>
+  );
 }
