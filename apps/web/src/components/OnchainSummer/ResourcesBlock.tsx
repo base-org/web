2a8975db92c37bdff45ef8dd5b51512eedcef21b
Matthew Bunday <matthew.bunday@coinbase.com>
2024-05-16 10:22:50 -0400

    Adds Onchain Summer Landing page for builders (#496)
    * Scaffold OCS
    
    * Work on OCS hero
    
    * Add about section
    
    * get scroll banner working
    
    * Mobile responsive about and hero blocks
    
    * Checkpoint, britney setup, rewards block
    
    * Polish, responsive design, hover states
    
    * join us onchain random leter
    
    * Events block
    
    * Sponsors block sans sponsor cards
    
    * CommunityEventsBlock
    
    * Checkpoint, content except sponsor cards and links finished
    
    * Add sponsors
    
    * Add sponsor logos
    
    * Hover state polish
    
    * Lint
    
    * Add remaining links
    
    * Adding motion
    
    * Design polish with Shelley
    
    * Fix mobile
    
    * Fix devfolio link
    
    * Fix retropgf link
    
    * Update buildathon copy
    
    * Undo about page change
    
    * Requested changes
    
    * Copy changes
    
    * Add OCS banner
    
    * Change hover durations
    
    * Small copy change
    
    * Move url to buildersummer
    
    * Fix eslint warnings
    
    * Remove button fade

---
+++ apps/web/src/components/OnchainSummer/ResourcesBlock.tsx
@@ -0,0 +1,85 @@
+import Brit from 'apps/web/src/components/OnchainSummer/Brit';
+import { BlackCircle } from 'apps/web/src/components/OnchainSummer/Circles';
+import { FadeInSection } from 'apps/web/src/components/OnchainSummer/FadeIns';
+
+function ResourcesCard({
+  title,
+  description,
+  link,
+  linkText,
+}: {
+  title: string;
+  description: string;
+  link?: string;
+  linkText?: string;
+}) {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      rel="noreferrer noopener"
+      className="flex min-h-[240px] flex-col justify-between gap-2 rounded-[5px] border-[1.5px] border-solid border-blue-600 p-4 duration-200 hover:bg-ocsblue hover:text-white lg:min-w-[200px]"
+    >
+      <div className="flex flex-col">
+        <span className="mb-8 font-display text-2xl font-light uppercase md:text-4xl">{title}</span>
+      </div>
+      <div className="flex flex-col">
+        <div className="text-lg">{description}</div>
+        <div className="text my-4 font-mono uppercase">[→] {linkText}</div>
+      </div>
+    </a>
+  );
+}
+
+export default function ResourcesBlock() {
+  return (
+    <div className="my-12 flex w-full max-w-[1200px] flex-col px-8">
+      <div className="text-l flex flex-row items-center gap-2 font-mono uppercase">
+        <BlackCircle /> Explore more
+      </div>
+      <FadeInSection>
+        <div className="my-6 flex flex-col gap-6">
+          <span className="text-5xl font-extrabold leading-9 md:text-7xl">
+            <Brit axis={147}>g</Brit>ET STARTE<Brit axis={147}>d</Brit>
+          </span>
+          <p className="mt-4 text-2xl">Docs, demos, and resources to help you get going.</p>
+        </div>
+      </FadeInSection>
+
+      <FadeInSection delay={0.5}>
+        <section className="mb-6 mt-6 grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+          <ResourcesCard
+            title="Base Docs"
+            description="Everything from network information to tutorials"
+            linkText="Learn more"
+            link="https://docs.base.org/?utm_source=base&utm_medium=web&utm_campaign=onchainsummer"
+          />
+          <ResourcesCard
+            title="Base Camp"
+            description="Learn smart contract development"
+            linkText="Learn more"
+            link="https://docs.base.org/base-camp/docs/welcome?utm_source=base&utm_medium=web&utm_campaign=onchainsummer"
+          />
+          <ResourcesCard
+            title="Build Onchain Apps Template"
+            description="Save time when building onchain experiences"
+            linkText="Learn more"
+            link="https://buildonchainapps.xyz/?utm_source=base&utm_medium=web&utm_campaign=onchainsummer"
+          />
+          <ResourcesCard
+            title="Onchain Kit"
+            description="React components and TypeScript utilities to build top-tier onchain apps"
+            linkText="Learn more"
+            link="https://onchainkit.xyz/?utm_source=base&utm_medium=web&utm_campaign=onchainsummer"
+          />
+          <ResourcesCard
+            title="Smart Wallet"
+            description="Integrate the SDK today and learn about Base credits"
+            linkText="Learn more"
+            link="https://www.smartwallet.dev/why?utm_source=base&utm_medium=web&utm_campaign=onchainsummer"
+          />
+        </section>
+      </FadeInSection>
+    </div>
+  );
+}
---
+++ apps/web/src/components/OnchainSummer/SponsorsBlock.tsx
@@ -0,0 +1,116 @@
+import Brit from 'apps/web/src/components/OnchainSummer/Brit';
+import { BlackCircle } from 'apps/web/src/components/OnchainSummer/Circles';
+import stripe from 'apps/web/public/images/ocs/sponsors/stripe.png';
+import farcaster from 'apps/web/public/images/ocs/sponsors/farcaster.png';
+import aerodrome from 'apps/web/public/images/ocs/sponsors/aerodrome.png';
+import fleek from 'apps/web/public/images/ocs/sponsors/fleek.png';
+import thirdweb from 'apps/web/public/images/ocs/sponsors/thirdweb.png';
+import zora from 'apps/web/public/images/ocs/sponsors/zora.png';
+import shopify from 'apps/web/public/images/ocs/sponsors/shopify.png';
+import synthetix from 'apps/web/public/images/ocs/sponsors/synthetix.png';
+import Image from 'next/image';
+import { FadeInSection } from 'apps/web/src/components/OnchainSummer/FadeIns';
+
+const sponsors = [
+  {
+    name: 'Stripe',
+    color: '#635BFF',
+    text: 'white',
+    image: stripe,
+  },
+  {
+    name: 'Farcaster',
+    color: '#833DFF',
+    text: 'white',
+    image: farcaster,
+  },
+  {
+    name: 'Aerodrome',
+    color: '#E9E8E3',
+    text: 'black',
+    image: aerodrome,
+  },
+  {
+    name: 'Fleek',
+    color: '#0A0C0E',
+    text: 'white',
+    image: fleek,
+  },
+  {
+    name: 'Thirdweb',
+    color: '#DC35A7',
+    text: 'white',
+    image: thirdweb,
+  },
+  {
+    name: 'Zora',
+    color: '#E9E8E3',
+    text: 'black',
+    image: zora,
+  },
+  {
+    name: 'Shopify',
+    color: '#5E8E3E',
+    text: 'white',
+    image: shopify,
+  },
+  // {
+  //   name: 'Nouns',
+  //   color: '#6EC8FA',
+  // },
+  {
+    name: 'Synthetix',
+    color: '#00BAFA',
+    text: 'white',
+    image: synthetix,
+  },
+];
+
+export default function SponsorsBlock() {
+  return (
+    <div className="my-20 flex w-full max-w-[1200px] flex-col">
+      <div className="text-l flex flex-row items-center gap-2 px-8 font-mono uppercase">
+        <BlackCircle /> Sponsors
+      </div>
+      <FadeInSection>
+        <div className="my-6 flex flex-col gap-6 px-8">
+          <span className="text-5xl font-extrabold leading-9 md:text-7xl">
+            BUILDATION TR<Brit>a</Brit>CK <Brit axis={68}>s</Brit>PONSORS
+          </span>
+          <p className="mt-4 text-2xl md:text-4xl">
+            8 tracks across commerce, payments, gaming, creator, social, and more. Sponsored by
+            leading builders and innovators
+          </p>
+        </div>
+      </FadeInSection>
+      <FadeInSection delay={0.5}>
+        <div
+          className="mb-8 flex w-full max-w-[1200px] flex-row gap-2 overflow-x-auto pb-6 md:px-8"
+          style={{
+            scrollbarWidth: 'thin',
+            scrollbarColor: '#000',
+          }}
+        >
+          {sponsors.map((sponsor) => (
+            <div
+              key={sponsor.name}
+              className="flex min-h-[150px] min-w-[150px] flex-col justify-between gap-2 rounded-[3px] p-4"
+              style={{ background: sponsor.color }}
+            >
+              <Image src={sponsor.image} alt={sponsor.name} height={50} />
+              <span
+                className="text-l font-mono font-light uppercase"
+                style={{ color: sponsor.text }}
+              >
+                {sponsor.name}
+              </span>
+            </div>
+          ))}
+        </div>
+        <span className="px-8">SCROLL &gt;</span>
+        <div className="ml-8 h-[3px] w-[90px] bg-black px-8" />
+        <div className="mx-8 h-[1px] w-[300px] bg-black px-8" />
+      </FadeInSection>
+    </div>
+  );
+}
