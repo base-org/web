65cf2565a9320ccc3dc55ff6664bb65e8b8dbb91
Ricardo Moguel <ricardo.moguel@coinbase.com>
2024-06-11 11:49:52 -0700

    update infra to single kv

--- apps/web/pages/api/checkProof/index.ts
+++ apps/web/pages/api/checkProof/index.ts
@@ -1,33 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { createClient, VercelKV } from '@vercel/kv';
+import { kv } from '@vercel/kv';
 
 enum ProofProjectType {
-  NFTBuilder = 'NFTBuilder',
-  Usernames = 'Usernames',
+  NFTBuilder = 'nft_builder',
+  Usernames = 'usernames',
 }
 
 type RequestBody = {
   address: `0x${string}`;
   projectType?: ProofProjectType;
 };
-
-const kvConfigs = {
-  NFTBuilder: {
-    url: process.env.KV_REST_API_URL ?? '',
-    token: process.env.KV_REST_API_TOKEN ?? '',
-  },
-  Usernames: {
-    url: process.env.KV_USERNAMES_REST_API_URL ?? '',
-    token: process.env.KV_USERNAMES_REST_API_TOKEN ?? '',
-  },
-};
-
-function getKVClient(projectType?: ProofProjectType): VercelKV {
-  if (!projectType || !kvConfigs[projectType]) {
-    throw new Error(`Unsupported project type: ${projectType}`);
-  }
-  return createClient(kvConfigs[projectType]);
-}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
@@ -37,9 +19,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const { address, projectType } = req.body as RequestBody;
 
-  const kvClient = getKVClient(projectType);
   try {
-    const proof = await kvClient.get<string[]>(`proof:${address}`);
+    let key = `proof:${projectType?.valueOf()}:${address}`;
+    if (ProofProjectType.Usernames === projectType) {
+      key = `proof:${address}`;
+    }
+    const proof = await kv.get<string[]>(key);
     if (proof) {
       return res.status(200).json({ result: proof });
     }
