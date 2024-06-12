3b121373844f6dc50919eeb154768e748951b601
Matthew Bunday <matthew.bunday@coinbase.com>
2024-06-12 13:56:27 -0400

    clean up nit

--- apps/web/src/components/ShinyButton/ShinyButton.tsx
+++ apps/web/src/components/ShinyButton/ShinyButton.tsx
@@ -14,12 +14,6 @@ type ShinyButtonProps = {
   children: ReactNode;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-/*
-    <button class="before:ease relative h-12 w-40 overflow-hidden border border-green-500 bg-green-500 text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-green-500 hover:before:-translate-x-40">
-      <span relative="relative z-10">Shine</span>
-    </button>
-*/
-
 export function ShinyButton({
   variant = 'white',
   children,
