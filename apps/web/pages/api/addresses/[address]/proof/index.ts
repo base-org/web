17b57504d7f9f81f70fed6ebc98e6bd57f212ddd
moggr <ricardo.mogg@gmail.com>
2024-06-18 10:41:54 -0700

    Update apps/web/pages/api/addresses/[address]/proof/index.ts
    Co-authored-by: Jordan Frankfurt <jordanwfrankfurt@gmail.com>

--- apps/web/pages/api/addresses/[address]/proof/index.ts
+++ apps/web/pages/api/addresses/[address]/proof/index.ts
@@ -3,8 +3,7 @@ import { getProofsByNamespaceAndAddress, ProofTableNamespace } from 'apps/web/sr
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
-    res.status(405).json({ error: 'method not allowed' });
-    return;
+    return res.status(405).json({ error: 'method not allowed' });
   }
   const { address, namespace } = req.query;
   if (!address) {
