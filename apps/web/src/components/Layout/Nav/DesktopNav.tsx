c26a5c08cdd1fc57791bb610e3873a3319d8ae8e
wbnns <hello@wbnns.com>
2024-06-18 11:08:07 +0000

    fix(block explorer): Point to Blockscout alongside outage (#571)
    Basescan is currently experiencing an outage. This updates relevant
    block explorer links to point to Blockscout in the interim.

--- apps/web/src/components/Layout/Nav/DesktopNav.tsx
+++ apps/web/src/components/Layout/Nav/DesktopNav.tsx
@@ -148,7 +148,7 @@ function DesktopNav({ color }: DesktopNavProps) {
         <DropdownLink href={docsUrl} label="Docs" color={color} externalLink />
         <DropdownLink href="https://base.org/camp" label="Learn" color={color} externalLink />
         <DropdownLink
-          href="https://explorer.base.org/"
+          href="https://base.blockscout.com/"
           label="Block Explorer"
           color={color}
           externalLink
