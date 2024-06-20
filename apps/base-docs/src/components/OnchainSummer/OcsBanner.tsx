5f87d8874029b6e9320148bc1463b315ba22d25b
Brendan from DeFi <brendan.forster@coinbase.com>
2024-06-20 10:34:14 -0700

    Feat: OcsBanner added to docs.base.org (#579)
    * started work on OcsBanner for docs.base.org
    
    * matched banner style to implementation on base.org
    
    * Added domain to href, fixed anchor style on hover
    
    * updated designs and removed unnecessary isOnPage logic

---
+++ apps/base-docs/src/components/OnchainSummer/OcsBanner.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useLocalStorage } from 'usehooks-ts';
+import { useCallback } from 'react';
+import Icon from '../Icon';
+
+import styles from './styles.module.css';
+
+const href = 'https://base.org/onchainsummer?utm_source=dotorg&utm_campaign=onchainsummer';
+
+export function OcsBanner() {
+  const [isBannerVisible, setIsBannerVisible] = useLocalStorage('isOcsBannerVisible', true);
+
+  const hideBanner = useCallback(() => {
+    setIsBannerVisible(false);
+  }, [setIsBannerVisible]);
+
+  if (!isBannerVisible) {
+    return null;
+  }
+
+  return (
+    <div className={styles.bannerContainer}>
+      <div className={styles.bannerInner}>
+        <a
+            href={href}
+            target="_blank"
+            rel="noreferrer"
+            aria-label="Onchain Summer Buildathon Banner"
+        >
+            <span className={styles.bannerText}>Join the Onchain Summer Buildathon!</span>
+        </a>
+        <div className={styles.bannerIconContainer}>
+            <a
+                href={href}
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Onchain Summer Buildathon Banner"
+            >
+                <span className={styles.bannerSpacer} />
+            </a>
+          <button
+            className={styles.bannerIconButton}
+            onClick={hideBanner}
+            onKeyDown={hideBanner}
+            type="button"
+          >
+            <Icon name="close" width="16" height="16" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
