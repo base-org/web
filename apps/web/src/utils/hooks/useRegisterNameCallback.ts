f33e6f1198de3202562e24f3b698eab411731ae4
Jordan Frankfurt <jordan.frankfurt@coinbase.com>
2024-06-20 19:38:09 -0500

    wip name registration callback

--- apps/web/src/utils/hooks/useRegisterNameCallback.ts
+++ apps/web/src/utils/hooks/useRegisterNameCallback.ts
@@ -1,7 +1,7 @@
 import abi from 'apps/web/src/abis/RegistrarControllerABI.json';
-import { formatNameForEns } from 'apps/web/src/utils/usernames';
+import { getContract } from 'viem';
 import { normalize } from 'viem/ens';
-import { useAccount, useWriteContract } from 'wagmi';
+import { useAccount, useWalletClient, useWriteContract } from 'wagmi';
 import { baseSepolia } from 'wagmi/chains';
 
 function secondsInYears(years: number): number {
@@ -11,24 +11,47 @@ function secondsInYears(years: number): number {
 
 export function useRegisterNameCallback(name: string, years: number) {
   const { address } = useAccount();
-  const formattedName = formatNameForEns(name);
-  const normalizedName = normalize(formattedName);
-  const { writeContract } = useWriteContract();
+  const normalizedName = normalize(name);
+  const { data: client } = useWalletClient();
   const registerRequest = {
     name: normalizedName, // The name being registered.
     owner: address, // The address of the owner for the name.
     duration: secondsInYears(years), // The duration of the registration in seconds.
-    resolver: '', // The address of the resolver to set for this name.
-    data: [], //  Multicallable data bytes for setting records in the associated resolver upon reigstration.
+    resolver: '0x51A16746Af2247DCA3665c078cCCf5678d19E366', // The address of the resolver to set for this name.
+    data: new Uint8Array(32).fill(0x0), //  Multicallable data bytes for setting records in the associated resolver upon reigstration.
     reverseRecord: true, // Bool to decide whether to set this name as the "primary" name for the `owner`.
   };
-
-  return () =>
-    writeContract({
+  if (client) {
+    const controllerContract = getContract({
       abi,
       address: '0xc8b5d24753588fc7ed134df8870f9d5544a3836e',
-      functionName: 'discountedRegister',
-      args: [registerRequest, 'discountKey', 'validationData'],
-      chainId: baseSepolia.id,
+      client: client,
     });
+    controllerContract.write
+      .discountedRegister([registerRequest, 0x0, 0x0])
+      .then(console.log)
+      .catch(console.error);
+  }
+  const { writeContractAsync } = useWriteContract();
+
+  // discountKey <- getValidDiscounts(): DiscountDetails[]
+  // for each discount in DiscountDetails, check if user is valid (?)
+  // handler for discount type
+
+  // isValidDiscountedRegistration()
+  return async () => {
+    try {
+      console.log('jf registerRequest', registerRequest);
+      const result = await writeContractAsync({
+        abi,
+        address: '0xc8b5d24753588fc7ed134df8870f9d5544a3836e',
+        functionName: 'discountedRegister',
+        args: [registerRequest, 0x0, 0x0],
+        chainId: baseSepolia.id,
+      });
+      console.log('jf result', result);
+    } catch (e) {
+      console.error('jf e', e);
+    }
+  };
 }
