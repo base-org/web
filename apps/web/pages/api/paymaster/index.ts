c1c206c6b03b8801dc7b74888c68d5f15d9e3c61
keshav <keshav17@berkeley.edu>
2024-06-25 14:48:56 -0700

    paymaster offchain service

---
+++ apps/web/pages/api/paymaster/index.ts
@@ -0,0 +1,25 @@
+import { paymasterClient } from "../paymaster_config";
+import { willSponsor } from "../../../src/utils/paymaster_sponsor";
+
+export async function POST(r: Request) {
+    const req = await r.json();
+    const method = req.method;
+    const [userOp, entrypoint, chainId] = req.params;
+    console.log(req.params);
+    if (!willSponsor({ chainId: parseInt(chainId), entrypoint, userOp })) {
+      return Response.json({ error: "Not a sponsorable operation" });
+    }
+  
+    if (method === "pm_getPaymasterStubData") {
+      const result = await paymasterClient.getPaymasterStubData({
+        userOperation: userOp,
+      });
+      return Response.json({ result });
+    } else if (method === "pm_getPaymasterData") {
+      const result = await paymasterClient.getPaymasterData({
+        userOperation: userOp,
+      });
+      return Response.json({ result });
+    }
+    return Response.json({ error: "Method not found" });
+  }
