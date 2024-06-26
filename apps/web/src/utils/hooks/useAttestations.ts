9a5c63274af71ed352159c7f467e0180b78f16b3
Jordan Frankfurt <jordan.frankfurt@coinbase.com>
2024-06-26 13:51:09 -0500

    use chain query param

--- apps/web/src/utils/hooks/useAttestations.ts
+++ apps/web/src/utils/hooks/useAttestations.ts
@@ -2,9 +2,11 @@ import { CBIDProofResponse } from 'apps/web/pages/api/proofs/cbid';
 import { CoinbaseProofResponse } from 'apps/web/pages/api/proofs/coinbase';
 import { ProofTableNamespace } from 'apps/web/src/utils/proofs';
 import { useEffect, useState } from 'react';
+import { base, baseSepolia } from 'viem/chains';
+import { useAccount } from 'wagmi';
 
 export function useCheckCBIDAttestations() {
-  // const { address } = useAccount();
+  const { chainId } = useAccount();
   const address = '0xB18e4C959bccc8EF86D78DC297fb5efA99550d85';
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<CBIDProofResponse | null>(null);
@@ -13,9 +15,11 @@ export function useCheckCBIDAttestations() {
     async function checkCBIDAttestations() {
       try {
         setLoading(true);
-        const response = await fetch(
-          `/api/proofs/cbid?address=${address}&namespace=${ProofTableNamespace.Usernames}`,
-        );
+        const params = new URLSearchParams();
+        params.append('address', address);
+        params.append('namespace', ProofTableNamespace.Usernames);
+        params.append('chain', (chainId === baseSepolia.id ? chainId : base.id).toString());
+        const response = await fetch(`/api/proofs/cbid?${params}`);
         const result = (await response.json()) as CBIDProofResponse;
         setData(result);
       } catch (e) {
@@ -28,13 +32,13 @@ export function useCheckCBIDAttestations() {
     if (address) {
       checkCBIDAttestations().catch(console.error);
     }
-  }, [address]);
+  }, [address, chainId]);
 
   return { data, loading };
 }
 
 export function useCheckCoinbaseAttestations() {
-  // const { address } = useAccount();
+  const { chainId } = useAccount();
   const address = '0xB18e4C959bccc8EF86D78DC297fb5efA99550d85';
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<CoinbaseProofResponse | null>(null);
@@ -43,7 +47,10 @@ export function useCheckCoinbaseAttestations() {
     async function checkCoinbaseAttestations() {
       try {
         setLoading(true);
-        const response = await fetch(`/api/proofs/coinbase?address=${address}`);
+        const params = new URLSearchParams();
+        params.append('address', address);
+        params.append('chain', (chainId === baseSepolia.id ? chainId : base.id).toString());
+        const response = await fetch(`/api/proofs/coinbase?${params}`);
         const result = (await response.json()) as CoinbaseProofResponse;
         setData(result);
       } catch (e) {
@@ -56,7 +63,7 @@ export function useCheckCoinbaseAttestations() {
     if (address) {
       checkCoinbaseAttestations().catch(console.error);
     }
-  }, [address]);
+  }, [address, chainId]);
 
   return { data, loading };
 }
