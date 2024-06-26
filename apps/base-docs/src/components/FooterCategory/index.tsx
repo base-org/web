e14b3964c267bddcd8b2fbaaf66031240a4eca46
Brendan Forster <brendan.forster@coinbase.com>
2024-06-26 14:32:05 -0700

    Updated FooterCategory component for analytics events

--- apps/base-docs/src/components/FooterCategory/index.tsx
+++ apps/base-docs/src/components/FooterCategory/index.tsx
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
+import logEvent, { AnalyticsEventImportance, CCAEventData } from 'base-ui/utils/logEvent';
 import styles from './styles.module.css';
 
-type FooterLink = {
+type FooterLinkType = {
   title: string;
   href: string;
+  analyticsData: {
+    name: string;
+    event: CCAEventData;
+    importance: AnalyticsEventImportance;
+  };
 };
 
 type FooterCategoryProps = {
   title: string;
-  links: FooterLink[];
+  links: FooterLinkType[];
 };
 
-function FooterCategory({ title, links }: FooterCategoryProps) {
+function FooterLink({ title, href, analyticsData }: FooterLinkType) {
+  const linkClick = useCallback(() => {
+    logEvent(analyticsData.name, analyticsData.event, analyticsData.importance);
+  }, [logEvent]);
+
+  return (
+    <li key={title} className={styles.footerCategoryListItem}>
+      <a href={href} className={styles.footerCategoryLink} onClick={linkClick}>
+        {title}
+      </a>
+    </li>
+  );
+}
+
+export default function FooterCategory({ title, links }: FooterCategoryProps) {
   return (
     <div className={styles.footerCategory}>
       <h4 className={styles.footerCategoryTitle}>{title}</h4>
       <ul className={styles.footerCategoryList}>
         {links.map((link) => (
-          <li key={link.title} className={styles.footerCategoryListItem}>
-            <a href={link.href} className={styles.footerCategoryLink}>
-              {link.title}
-            </a>
-          </li>
+          <FooterLink title={link.title} href={link.href} analyticsData={link.analyticsData} />
         ))}
       </ul>
     </div>
   );
 }
-
-export default FooterCategory;
