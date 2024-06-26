3b609a42f4ae6ca1f9a28ee649fa64f0c877e14f
Brendan Forster <brendan.forster@coinbase.com>
2024-06-26 14:09:15 -0700

    updated TextCard to send event analytics

--- apps/base-docs/src/components/TextCard/index.tsx
+++ apps/base-docs/src/components/TextCard/index.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Icon from '../Icon';
 
 import styles from './styles.module.css';
+import logEvent, { AnalyticsEventImportance, CCAEventData } from 'base-ui/utils/logEvent';
 
 type TextCardProps = {
   title: string;
   description: string;
   href: string;
+  analyticsData: {
+    name: string;
+    event: CCAEventData;
+    importance: AnalyticsEventImportance;
+  };
 };
 
-function TextCard({ title, description, href }: TextCardProps) {
+function TextCard({ title, description, href, analyticsData }: TextCardProps) {
+  const linkClick = useCallback(() => {
+    logEvent(analyticsData.name, analyticsData.event, analyticsData.importance);
+  }, [logEvent]);
   return (
     <div className={styles.textCard}>
       <div>
         <h3 className={styles.textCardTitle}>{title}</h3>
         <p className={styles.textCardDescription}>{description}</p>
       </div>
-      <a href={href} className={styles.textCardLink}>
+      <a href={href} className={styles.textCardLink} onClick={linkClick}>
         <span>Get started</span>
         <Icon name="arrow-right" width="16" height="16" />
       </a>
