3a8108e0d475ecc99a2a568cc51a19eafab3b707
Jordan Frankfurt <jordan.frankfurt@coinbase.com>
2024-06-26 11:40:25 -0500

    ensure uniform utilization of the network check for contract args construction

--- apps/web/src/utils/hooks/useIsNameAvailable.ts
+++ apps/web/src/utils/hooks/useIsNameAvailable.ts
@@ -1,26 +1,24 @@
 import abi from 'apps/web/src/abis/RegistrarControllerABI.json';
 import { USERNAME_REGISTRAR_CONTROLLER_ADDRESS } from 'apps/web/src/addresses/usernames';
 import { normalizeEnsDomainName } from 'apps/web/src/utils/usernames';
-import { base } from 'viem/chains';
-import { webSocket } from 'viem';
-import { createConfig, useChainId, useReadContract } from 'wagmi';
-
-const config = createConfig({
-  chains: [base],
-  transports: {
-    [base.id]: webSocket(),
-  },
-});
+import { useMemo } from 'react';
+import { base, baseSepolia } from 'viem/chains';
+import { useChainId, useReadContract } from 'wagmi';
 
 export function useIsNameAvailable(name: string) {
   const normalizedName = normalizeEnsDomainName(name);
   const chainId = useChainId();
+  const network = chainId === baseSepolia.id ? chainId : base.id;
+  const readContractArgs = useMemo(
+    () => ({
+      abi,
+      address: USERNAME_REGISTRAR_CONTROLLER_ADDRESS[network],
+      functionName: 'available',
+      args: [normalizedName],
+      chainId: network,
+    }),
+    [network, normalizedName],
+  );
 
-  return useReadContract({
-    abi,
-    address: USERNAME_REGISTRAR_CONTROLLER_ADDRESS[chainId],
-    functionName: 'available',
-    args: [normalizedName],
-    config,
-  });
+  return useReadContract(readContractArgs);
 }
