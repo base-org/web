6207888663913d2d274fc588f789babc89f4521c
Brendan from DeFi <brendan.forster@coinbase.com>
2024-06-27 09:03:30 -0700

    Feat/add more docs events (#603)
    * addressed linter issues on logEvent and typing
    
    * addressed linter issues on OCSBanner
    
    * added analytics events to builder essentials buttons
    
    * updated TextCard to send event analytics
    
    * added event data to text cards
    
    * Updated FooterCategory component for analytics events
    
    * added event analytics to footer links
    
    * deleted dead bridge-faq footer link
    
    * created type for AnalyticsEventData
    
    * Updated type for AnalyticsEventData

--- apps/base-docs/src/pages/index.tsx
+++ apps/base-docs/src/pages/index.tsx
@@ -6,6 +6,7 @@ import TextCard from '../components/TextCard';
 import FooterCategory from '../components/FooterCategory';
 
 import styles from './styles.module.css';
+import { ActionType, AnalyticsEventImportance, ComponentType } from 'base-ui/utils/logEvent';
 
 const imageCards = [
   {
@@ -14,6 +15,15 @@ const imageCards = [
     description: 'Elevate your coding skills on Base with guided development tutorials.',
     buttonText: 'Get started',
     buttonHref: '/tutorials',
+    analyticsData: {
+      name: 'tutorials',
+      event: {
+        action: ActionType.click,
+        componentType: ComponentType.button,
+        context: 'builder_essentials',
+      },
+      importance: AnalyticsEventImportance.high,
+    },
   },
   {
     src: '/img/home-intro-2.png',
@@ -22,6 +32,15 @@ const imageCards = [
       'Become an onchain developer with our comprehensive smart contract development curriculum.',
     buttonText: 'Explore',
     buttonHref: '/base-camp/docs/welcome',
+    analyticsData: {
+      name: 'camp_bootcamp',
+      event: {
+        action: ActionType.click,
+        componentType: ComponentType.button,
+        context: 'builder_essentials',
+      },
+      importance: AnalyticsEventImportance.high,
+    },
   },
   {
     src: '/img/home-intro-3.png',
@@ -30,6 +49,15 @@ const imageCards = [
       'Innovate and build on Base to qualify for grants that support impactful projects.',
     buttonText: 'Learn more',
     buttonHref: 'https://paragraph.xyz/@grants.base.eth/calling-based-builders',
+    analyticsData: {
+      name: 'ecosystem_grants',
+      event: {
+        action: ActionType.click,
+        componentType: ComponentType.button,
+        context: 'builder_essentials',
+      },
+      importance: AnalyticsEventImportance.high,
+    },
   },
 ];
 
@@ -38,23 +66,59 @@ const textCards = [
     title: 'Deploying a smart contract',
     description: 'Learn how to deploy a smart contract to Base.',
     href: '/tutorials/deploy-with-foundry',
+    analyticsData: {
+      name: 'deploy_a_smart_contract',
+      event: {
+        action: ActionType.click,
+        componentType: ComponentType.link,
+        context: 'expert_how_to',
+      },
+      importance: AnalyticsEventImportance.high,
+    },
   },
   {
     title: 'Running a Base node',
     description: 'Learn how to set up and run your own local Base node.',
     href: '/tutorials/run-a-base-node',
+    analyticsData: {
+      name: 'run_a_node',
+      event: {
+        action: ActionType.click,
+        componentType: ComponentType.link,
+        context: 'expert_how_to',
+      },
+      importance: AnalyticsEventImportance.high,
+    },
   },
   {
     title: 'Build an onchain app',
     description:
       'Learn how to create an NFT collection and build an NFT gallery application for viewing metadata.',
     href: '/tutorials/build-with-thirdweb',
+    analyticsData: {
+      name: 'build_onchain_apps',
+      event: {
+        action: ActionType.click,
+        componentType: ComponentType.link,
+        context: 'expert_how_to',
+      },
+      importance: AnalyticsEventImportance.high,
+    },
   },
   {
     title: 'Access data using an Oracle',
     description:
       'Learn how to access price data within a smart contract using Chainlink price feeds.',
     href: '/tutorials/oracles-chainlink-price-feeds',
+    analyticsData: {
+      name: 'access_data_using_oracles',
+      event: {
+        action: ActionType.click,
+        componentType: ComponentType.link,
+        context: 'expert_how_to',
+      },
+      importance: AnalyticsEventImportance.high,
+    },
   },
 ];
 
@@ -65,30 +129,80 @@ const footerCategories = [
       {
         title: 'About Base',
         href: '/docs/',
+        analyticsData: {
+          name: 'intro_about_base',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
       {
         title: 'Using Base',
         href: '/docs/using-base',
+        analyticsData: {
+          name: 'intro_using_base',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
       {
         title: 'Network Information',
         href: '/docs/network-information',
+        analyticsData: {
+          name: 'intro_network_info',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
       {
         title: 'Base Contracts',
         href: '/docs/base-contracts',
+        analyticsData: {
+          name: 'intro_base_contracts',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
       {
         title: 'Fees',
         href: '/docs/fees',
+        analyticsData: {
+          name: 'intro_base_fees',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
       {
         title: 'Differences between Ethereum and Base',
         href: '/docs/differences',
-      },
-      {
-        title: 'Bridge FAQ',
-        href: '/docs/tools/bridge-faq',
+        analyticsData: {
+          name: 'intro_differences_ethereum_base',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
     ],
   },
@@ -98,30 +212,93 @@ const footerCategories = [
       {
         title: 'Node Providers',
         href: '/docs/tools/node-providers',
+        analyticsData: {
+          name: 'tools_node_providers',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
       {
         title: 'Block Explorers',
         href: '/docs/tools/block-explorers',
+        analyticsData: {
+          name: 'tools_block_explorers',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
       {
         title: 'Network Faucets',
         href: '/docs/tools/network-faucets',
+        analyticsData: {
+          name: 'tools_network_faucets',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
       {
         title: 'Oracles',
         href: '/docs/tools/oracles',
+        analyticsData: {
+          name: 'tools_oracles',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
       {
         title: 'Data Indexers',
         href: '/docs/tools/data-indexers',
+        analyticsData: {
+          name: 'tools_data_indexers',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
       {
         title: 'Cross-chain',
         href: '/docs/tools/cross-chain',
+        analyticsData: {
+          name: 'tools_cross_chain',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
       {
         title: 'Account Abstraction',
         href: '/docs/tools/account-abstraction',
+        analyticsData: {
+          name: 'tools_account_abstraction',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
     ],
   },
@@ -131,26 +308,80 @@ const footerCategories = [
       {
         title: 'Bridge',
         href: 'https://bridge.base.org',
+        analyticsData: {
+          name: 'resources_bridge',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
       {
         title: 'Token List',
         href: '/docs/tokens/list',
+        analyticsData: {
+          name: 'resources_token_list',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
       {
         title: 'Contracts',
         href: '/docs/contracts',
+        analyticsData: {
+          name: 'resources_contracts',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
       {
         title: 'Bug Bounty',
         href: 'https://hackerone.com/coinbase',
+        analyticsData: {
+          name: 'resources_bug_bounty',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
       {
         title: 'Status',
         href: 'https://status.base.org',
+        analyticsData: {
+          name: 'resources_status',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
       {
         title: 'Brand Kit',
         href: 'https://github.com/base-org/brand-kit',
+        analyticsData: {
+          name: 'resources_brand_kit',
+          event: {
+            action: ActionType.click,
+            componentType: ComponentType.link,
+            context: 'explore_documentation',
+          },
+          importance: AnalyticsEventImportance.high,
+        },
       },
     ],
   },
@@ -177,6 +408,7 @@ export default function Home() {
                 description={card.description}
                 buttonText={card.buttonText}
                 buttonHref={card.buttonHref}
+                analyticsData={card.analyticsData}
               />
             ))}
           </div>
@@ -189,6 +421,7 @@ export default function Home() {
                 key={card.title}
                 description={card.description}
                 href={card.href}
+                analyticsData={card.analyticsData}
               />
             ))}
           </div>
