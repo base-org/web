8cbf157c889f42b4734eba4b3c55368617ca26c6
Brendan Forster <brendan.forster@coinbase.com>
2024-07-11 14:56:21 -0700

    updated color references from yellow-70 (dark mode) to yellow-20 (light mode)

--- apps/web/src/components/Layout/Nav/OcsBanner.tsx
+++ apps/web/src/components/Layout/Nav/OcsBanner.tsx
@@ -19,8 +19,8 @@ export function OcsBanner() {
   }
 
   return (
-    <div className="z-10 flex w-full flex-row justify-center bg-yellow-70 text-black">
-      <div className="z-10 flex w-full max-w-[1440px] flex-row items-center justify-between self-center bg-yellow-70 p-2 pl-8 pr-6">
+    <div className="z-10 flex w-full flex-row justify-center bg-yellow-20 text-black">
+      <div className="z-10 flex w-full max-w-[1440px] flex-row items-center justify-between self-center bg-yellow-20 p-2 pl-8 pr-6">
         <Link href={href}>
           <span className="text-xs  md:text-base">Build Onchain this Summer!</span>
         </Link>
