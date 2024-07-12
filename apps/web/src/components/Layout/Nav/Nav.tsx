468c3a1c662542691b6ca90eb18da9f0324a7717
Brendan Forster <brendan.forster@coinbase.com>
2024-07-12 14:36:13 -0700

    Implemented Banner for OnchainKit

--- apps/web/src/components/Layout/Nav/Nav.tsx
+++ apps/web/src/components/Layout/Nav/Nav.tsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 import { Logo } from '../../Logo/Logo';
 import DesktopNav from './DesktopNav';
 import MobileMenu from './MobileMenu';
-import { OcsBanner } from 'apps/web/src/components/Layout/Nav/OcsBanner';
+import Banner from 'base-ui/components/Layout/Nav/Banner'
 
 type NavProps = {
   color: 'white' | 'black';
@@ -15,7 +15,7 @@ export function Nav({ color }: NavProps) {
 
   return (
     <>
-      <OcsBanner />
+      <Banner href='https://onchainkit.xyz/?utm_source=basedotorg&utm_medium=banner' text="Build on Base in minutes with OnchainKit!" />
       <nav className="bg-transparent z-10 flex h-24 w-full max-w-[1440px] flex-row items-center justify-between gap-16 self-center p-8">
         <Link href="/" aria-label="Base Homepage">
           <Logo color={color} path={pathname} width="106px" />
