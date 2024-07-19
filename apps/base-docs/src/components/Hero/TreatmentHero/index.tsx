8ed8b5dc12124d2dd4cc61c8e171c79f93e4415d
Brendan Forster <brendan.forster@coinbase.com>
2024-07-19 15:29:57 -0700

    modularized TreatmentHero

---
+++ apps/base-docs/src/components/Hero/TreatmentHero/index.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+
+import BarChart from './BarChart';
+import styles from './styles.module.css';
+
+export default function TreatmentHero() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', handleWindowResize);
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
+
+  if (windowWidth <= 500) {
+    return <MobileHero />;
+  }
+  return <DesktopHero />;
+}
+
+function DesktopHero() {
+  return (
+    <header className={styles.heroContainer}>
+      <div>
+        <h1 className={styles.heroTitle}>Build onchain</h1>
+      </div>
+      <div className={styles.chartContainer}>
+        <BarChart />
+      </div>
+      <div className={styles.secondaryContentContainer}>
+        <div>
+          <div className={styles.ctaText}>
+            Keep your costs low and save on gas with every transaction when you build on Base.
+          </div>
+          <button className={styles.ctaButton}>
+            <a href="/docs">LEARN MORE</a>
+          </button>
+        </div>
+        <div className={styles.secondaryTitleContainer}>
+          <h1 className={styles.heroTitle}>for less</h1>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+function MobileHero() {
+  return (
+    <header className={styles.heroContainer}>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.heroTitle}>Build onchain for less</h1>
+      </div>
+      <div className={styles.chartContainerMobile}>
+        <BarChart />
+      </div>
+      <div className={styles.secondaryContentContainerMobile}>
+        <div className={styles.ctaContainer}>
+          <div className={styles.ctaText}>
+            Keep your costs low and save on gas with every transaction when you build on Base.
+          </div>
+          <button className={styles.ctaButton}>
+            <a href="/docs">LEARN MORE</a>
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
