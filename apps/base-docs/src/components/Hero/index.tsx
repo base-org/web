ad0bd6e078c45cfa58e12772faf2db2b7574bdd5
Brendan Forster <brendan.forster@coinbase.com>
2024-07-23 11:06:27 -0700

    Added experiment logic to Hero

--- apps/base-docs/src/components/Hero/index.tsx
+++ apps/base-docs/src/components/Hero/index.tsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
+import { useExperiments } from 'base-ui/contexts/Experiments';
+
 import ControlHero from './ControlHero';
 import TreatmentHero from './TreatmentHero';
 
 export default function Hero() {
+  const { getUserVariant } = useExperiments();
+  const userVariant = getUserVariant('hero-build-onchain-for-less');
+  if (userVariant === 'treatment') {
+    return <TreatmentHero />;
+  }
   return <ControlHero />;
-  //   return <TreatmentHero />;
 }
