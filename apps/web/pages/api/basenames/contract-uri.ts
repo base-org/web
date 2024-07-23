62250a54b226970a5632513dd7fb160bb2bb5ce2
Léo Galley <contact@kirkas.ch>
2024-07-23 10:29:35 -0400

    add redirect when json is missing

--- apps/web/pages/api/basenames/contract-uri.ts
+++ apps/web/pages/api/basenames/contract-uri.ts
@@ -8,22 +8,10 @@ export const config = {
 export default async function GET(request: Request) {
   // TODO: Check this works in live/production
   const url = new URL(request.url);
-  const domainName = `${url.protocol}//${url.host}`;
-
   const chainId = url.searchParams.get('chainId') ?? base.id;
   if (!chainId) return NextResponse.json({ error: '406: chainId is missing' }, { status: 406 });
 
-  const name = Number(chainId) === base.id ? 'Basename' : 'Basename (Sepolia testnet)';
-
-  const tokenMedata = {
-    name: name,
-    description: 'TODO',
-    image: 'TODO', // TODO
-    banner_image: 'TODO.png', // TODO
-    featured_image: 'TODO.png', // TODO
-    external_link: `${domainName}/names`,
-    collaborators: [],
-  };
-
-  return NextResponse.json(tokenMedata);
+  return NextResponse.redirect(
+    new URL(`/api/basenames/contract-uri.json?chainId=${chainId}`, request.url),
+  );
 }
