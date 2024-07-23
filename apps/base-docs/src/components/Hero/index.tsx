ae44b04d1a33e8aa14b311310c36faca0d2fe088
Brendan Forster <brendan.forster@coinbase.com>
2024-07-23 14:35:10 -0700

    corrected the experiment_key value

--- apps/base-docs/src/components/Hero/index.tsx
+++ apps/base-docs/src/components/Hero/index.tsx
@@ -6,7 +6,7 @@ import ControlHero from './ControlHero';
 import TreatmentHero from './TreatmentHero';
 import styles from './styles.module.css';
 
-const EXPERIMENT_KEY = 'hero-build-onchain-for-less';
+const EXPERIMENT_KEY = 'build-onchain-for-less-hero-2024-07-23';
 
 export default function Hero() {
   const { isReady, getUserVariant } = useExperiments();
