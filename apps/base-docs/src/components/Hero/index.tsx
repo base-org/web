c27e3bb2e4f13da9eeeebcbf39c9b2e90c8e85fc
Brendan Forster <brendan.forster@coinbase.com>
2024-07-23 14:22:51 -0700

    refined loading state

--- apps/base-docs/src/components/Hero/index.tsx
+++ apps/base-docs/src/components/Hero/index.tsx
@@ -4,13 +4,20 @@ import { useExperiments } from 'base-ui/contexts/Experiments';
 
 import ControlHero from './ControlHero';
 import TreatmentHero from './TreatmentHero';
+import styles from './styles.module.css';
+
+const EXPERIMENT_KEY = 'hero-build-onchain-for-less';
 
 export default function Hero() {
   const { isReady, getUserVariant } = useExperiments();
-  const userVariant = getUserVariant('hero-build-onchain-for-less');
+  const userVariant = getUserVariant(EXPERIMENT_KEY);
 
   if (!isReady) {
-    return '';
+    return (
+      <div className={styles.loadingState}>
+        <div className={styles.loadingStateImg}></div>
+      </div>
+    );
   }
   if (userVariant === 'treatment') {
     return <TreatmentHero />;
