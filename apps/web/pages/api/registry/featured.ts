9a6e5c26231e3eb61363e3c8cc2e4f301003d079
moggr <ricardo.moguel@coinbase.com>
2024-07-24 10:23:05 -0700

    add request stats capturing on registry (#667)
    * add request stats capturing on registry
    
    * replace get/set to incr

--- apps/web/pages/api/registry/featured.ts
+++ apps/web/pages/api/registry/featured.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { db } from 'apps/web/src/utils/ocsRegistry';
+import { kv } from '@vercel/kv';
 
+const pageKey = 'api.ocs_registry.featured';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const content = await db
     .selectFrom('content')
@@ -14,6 +16,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const response = {
     data: row,
   };
+
+  try {
+    await kv.incr(`stat:requests.${pageKey}`);
+  } catch (error) {
+    console.error(error);
+  }
 
   // Set caching headers
   res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate');
