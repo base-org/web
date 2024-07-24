34f63e48398f690bc5e4291de51a4cd4738ed13d
Matthew Bunday <matthew.bunday@coinbase.com>
2024-07-24 14:08:41 -0400

    Remove CSP for talentprotocol

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -92,7 +92,6 @@ const contentSecurityPolicy = {
     'wss://www.walletlink.org',
     'https://base.easscan.org/graphql',
     'https://api.guild.xyz/',
-    'https://api.talentprotocol.com/',
     isLocalDevelopment ? 'ws://localhost:3000/' : '',
     isLocalDevelopment ? 'http://localhost:3000/' : '',
   ],
