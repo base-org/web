a9cdb038927fa6196945f91c37f3cae8ed60b895
Matthew Bunday <matthew.bunday@coinbase.com>
2024-07-26 14:11:50 -0400

    Chore/badge design polish (#688)
    * Replace PNGs with higher res webp
    
    * Design polish on badges
    
    * Update the icons
    
    * Extract props type
    
    * Use image with loading
    
    * tweak loading state and fix component
    
    * Add hover effect
    
    * Add badge count components and hover state
    
    ---------
    
    Co-authored-by: Léo Galley <contact@kirkas.ch>

--- apps/web/src/components/Basenames/UsernameProfileSectionTitle/index.tsx
+++ apps/web/src/components/Basenames/UsernameProfileSectionTitle/index.tsx
@@ -2,11 +2,11 @@ import { Icon } from 'apps/web/src/components/Icon/Icon';
 
 type UsernameProfileSectionTitleProps = {
   title: string;
-}
+};
 
 export default function UsernameProfileSectionTitle({ title }: UsernameProfileSectionTitleProps) {
   return (
-    <h3 className="flex flex items-center text-lg">
+    <h3 className="flex flex items-center text-lg font-medium">
       <span className="text-blue-600">
         <Icon name="blueCircle" color="currentColor" height="0.75rem" />
       </span>
