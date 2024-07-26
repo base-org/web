770ee5caf6c868eed4cbdb055789d2ff2c897801
Ricardo Moguel <ricardo.moguel@coinbase.com>
2024-07-25 21:23:03 -0700

    Automatically switch chain to base

--- apps/web/pages/names.tsx
+++ apps/web/pages/names.tsx
@@ -2,13 +2,26 @@ import AnalyticsProvider from 'apps/web/contexts/Analytics';
 import RegistrationProvider from 'apps/web/src/components/Basenames/RegistrationContext';
 import RegistrationFlow from 'apps/web/src/components/Basenames/RegistrationFlow';
 import { Layout, NavigationType } from 'apps/web/src/components/Layout/Layout';
+import { supportedChainIds } from 'apps/web/src/hooks/useBasenameChain';
 import Head from 'next/head';
-import { ReactElement } from 'react';
-
+import { ReactElement, useCallback } from 'react';
+import { base } from 'viem/chains';
+import { useSwitchChain, useChainId } from 'wagmi';
 // Do not change this unless you know what you're doing (it'll break analytics)
 const usernameRegistrationAnalyticContext = 'username_registration';
 
 export function Usernames() {
+  const chainId = useChainId();
+  const { switchChain } = useSwitchChain();
+
+  useCallback(() => {
+    if (!chainId || !switchChain) {
+      return;
+    }
+    if (!supportedChainIds.includes(chainId)) {
+      switchChain({ chainId: base.id });
+    }
+  }, [chainId, switchChain]);
   return (
     <>
       <Head>
