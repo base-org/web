1b7c3ce25c8be8ba8daad49c4be615c33ee2974a
Omkar Bhat <omkar1496@gmail.com>
2024-07-30 16:16:18 -0400

    use node_env

--- apps/web/app/datadog.ts
+++ apps/web/app/datadog.ts
@@ -10,7 +10,7 @@ datadogRum.init({
     // see https://docs.datadoghq.com/getting_started/site/
     site: 'datadoghq.com',
     service: 'base-org',
-    env: 'prod',
+    env: process.env.NODE_ENV,
     // Specify a version number to identify the deployed version of your application in Datadog
     // version: '1.0.0',
     sessionSampleRate: 100,
