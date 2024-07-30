54ae681431af137d3d8be21686c25b1a9fb58b08
Brendan Forster <brendan.forster@coinbase.com>
2024-07-30 11:15:48 -0700

    second pass evm compatible

--- apps/web/src/components/WhyBase/EvmEquivalent.tsx
+++ apps/web/src/components/WhyBase/EvmEquivalent.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 
 import React, { useCallback } from 'react';
 import Link from 'next/link';
@@ -26,16 +26,16 @@ export default function EvmEquivalent() {
 
   return (
     <div className="flex flex-row gap-32 bg-black px-20 pb-10 pt-10">
-      <div className="h-[320px] min-w-[550px] bg-ocsyellow">Placeholder</div>
-      <div className="flex flex-col justify-around">
+      <div className="h-[520px] min-w-[550px] bg-[url('../public/images/EVM-compatibility-chains.png')] bg-no-repeat" />
+      <div className="flex flex-col justify-center gap-16 max-w-[550px]">
         <h2 className="flex font-display text-5xl">
           <span>3.</span>
           <span className="ml-4">EVM Compatible for effortless migration</span>
         </h2>
         <div>
           <span>
-            Base leads the way with the lowest transaction costs, highest throughput, and top TPS
-            among Layer 2 solutions.
+            Move your project to Base in minutes from any other EVM-compatible chain and unlock the
+            full potential of the Superchain and OP stack
           </span>
           <Link
             href="https://docs.base.org/docs"
