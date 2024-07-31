ad5b1acce341f3f94bea33c081b3e8636607ed4d
Léo Galley <contact@kirkas.ch>
2024-07-31 16:12:18 -0400

    better datadog env-specific integration

--- apps/web/app/datadog.ts
+++ apps/web/app/datadog.ts
@@ -2,28 +2,39 @@
 'use client';
 
 import { datadogRum } from '@datadog/browser-rum';
+import { useEffect } from 'react';
 
-datadogRum.init({
-  applicationId: process.env.NEXT_PUBLIC_DATADOG_APP_ID ?? '',
-  clientToken: process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN ?? '',
-  // `site` refers to the Datadog site parameter of your organization
-  // see https://docs.datadoghq.com/getting_started/site/
-  site: 'datadoghq.com',
-  service: 'base-org',
-  env: process.env.NODE_ENV,
-  // Specify a version number to identify the deployed version of your application in Datadog
-  // version: '1.0.0',
-  sessionSampleRate: 100,
-  sessionReplaySampleRate: 20,
-  trackUserInteractions: true,
-  trackResources: true,
-  trackLongTasks: true,
-  defaultPrivacyLevel: 'mask',
-  // Specify URLs to propagate trace headers for connection between RUM and backend trace
-});
+const nextPublicDatadogAppId = process.env.NEXT_PUBLIC_DATADOG_APP_ID ?? '';
+const nextPublicDatadogClientToken = process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN ?? '';
 
 export default function DatadogInit() {
-  // Render nothing - this component is only included so that the init code
-  // above will run client-side
+  const isDevelopment = process.env.NODE_ENV === 'development';
+  useEffect(() => {
+    if (!nextPublicDatadogAppId || !nextPublicDatadogClientToken) {
+      if (isDevelopment) {
+        console.info('Development: Datadog is not configured');
+      } else {
+        console.warn('Datadog is not configured');
+      }
+
+      return;
+    }
+    datadogRum.init({
+      applicationId: process.env.NEXT_PUBLIC_DATADOG_APP_ID ?? '',
+      clientToken: process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN ?? '',
+      // `site` refers to the Datadog site parameter of your organization
+      // see https://docs.datadoghq.com/getting_started/site/
+      site: 'datadoghq.com',
+      service: 'base-org',
+      env: process.env.NODE_ENV,
+      sessionSampleRate: 100,
+      sessionReplaySampleRate: 20,
+      trackUserInteractions: true,
+      trackResources: true,
+      trackLongTasks: true,
+      defaultPrivacyLevel: 'mask',
+    });
+  }, [isDevelopment]);
+
   return null;
 }
