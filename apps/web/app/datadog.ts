8c1842032003f64900c9be890e34b89f0e927ef7
omkar <omkar1496@gmail.com>
2024-07-31 15:19:28 -0400

    Fix datadog env var name to be available on the FE (#764)

--- apps/web/app/datadog.ts
+++ apps/web/app/datadog.ts
@@ -4,8 +4,8 @@
 import { datadogRum } from "@datadog/browser-rum";
 
 datadogRum.init({
-    applicationId: process.env.DATADOG_APP_ID ?? "",
-    clientToken: process.env.DATADOG_CLIENT_TOKEN ?? "",
+    applicationId: process.env.NEXT_PUBLIC_DATADOG_APP_ID ?? "",
+    clientToken: process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN ?? "",
     // `site` refers to the Datadog site parameter of your organization
     // see https://docs.datadoghq.com/getting_started/site/
     site: 'datadoghq.com',
