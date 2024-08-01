dfc28b94d52b793cd5e89faac517b9ba64462cca
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2024-08-01 11:14:56 -0500

    drop i icon on mobile, add 0x suggestion to name prompt (#773)

--- apps/web/src/components/Tooltip/index.tsx
+++ apps/web/src/components/Tooltip/index.tsx
@@ -3,12 +3,13 @@ import { PropsWithChildren, ReactNode } from 'react';
 
 type TooltipProps = {
   content: ReactNode;
+  className?: string;
 };
 
-export default function Tooltip({ content, children }: PropsWithChildren<TooltipProps>) {
+export default function Tooltip({ content, children, ...rest }: PropsWithChildren<TooltipProps>) {
   return (
     <Root>
-      <Trigger>{children}</Trigger>
+      <Trigger {...rest}>{children}</Trigger>
       <Portal>
         <Content className="z-50 shadow-lg ring-1 ring-gray-dark ring-opacity-5">
           <Arrow />
