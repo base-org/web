a81b3ea4855a5fc47a9c0c1619174f47b8b3c738
Léo Galley <contact@kirkas.ch>
2024-08-01 09:05:05 -0400

    Add link to waitlist form

--- apps/web/src/components/Basenames/RegistrationForm/index.tsx
+++ apps/web/src/components/Basenames/RegistrationForm/index.tsx
@@ -23,6 +23,7 @@ import { useRegisterNameCallback } from 'apps/web/src/hooks/useRegisterNameCallb
 import { IS_EARLY_ACCESS } from 'apps/web/src/utils/usernames';
 import classNames from 'classnames';
 import { ActionType } from 'libs/base-ui/utils/logEvent';
+import Link from 'next/link';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { formatEther } from 'viem';
 import { useAccount, useBalance, useChains, useSwitchChain } from 'wagmi';
@@ -45,6 +46,8 @@ function formatUsdPrice(price: bigint, ethUsdPrice: number) {
   if (parsed === '0.00') return '0';
   return parsed;
 }
+
+const WAITLIST_FORM = 'https://app.deform.cc/form/6acf7a89-8cb5-4c31-b71d-7979014f4db4';
 
 export default function RegistrationForm() {
   const { isConnected, chain: connectedChain, address } = useAccount();
@@ -304,10 +307,19 @@ export default function RegistrationForm() {
   if (isConnected) {
     if (isOnSupportedNetwork) {
       return (
-        <div className="z-10 mt-8 flex flex-row items-center justify-center ">
-          <ExclamationCircleIcon width={12} height={12} className="fill-state-n-hovered" />
-          <p className="ml-2 text-state-n-hovered">
-            The connected wallet is not eligible for early access.
+        <div className="z-10 mt-8 flex flex-row items-center justify-center text-gray-40">
+          <p className="ml-2 text-center">
+            <span className="mr-2 inline-block">
+              <Icon name="info" width={12} height={12} color="currentColor" />
+            </span>
+            The connected wallet is not eligible for early access.{' '}
+            <Link
+              href={WAITLIST_FORM}
+              target="_blank"
+              className="text-blue-500 underline underline-offset-4"
+            >
+              Join the waitlist
+            </Link>
           </p>
         </div>
       );
