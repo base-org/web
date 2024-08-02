237a28f49b9af78ed4e1085962f82e3aaca7c0be
Brendan Forster <brendan.forster@coinbase.com>
2024-08-02 10:40:48 -0700

    refactored to use TableOfContents component

--- apps/web/src/components/WhyBase/Hero.tsx
+++ apps/web/src/components/WhyBase/Hero.tsx
@@ -1,4 +1,4 @@
-import Link from 'apps/web/node_modules/next/link';
+import TableOfContents, { Section } from 'apps/web/src/components/TableOfContents/TableOfContents';
 
 const headerReasons: Section[] = [
   {
@@ -25,10 +25,12 @@ const headerReasons: Section[] = [
 
 export default async function Hero() {
   return (
-    <div className="mx-12 sm:mx-16 lg:mx-24 mt-12 sm:mt-16 lg:mt-[96px] flex flex-col justify-start min-h-[504px]">
-      <div className="my-8 sm:my-12 lg:my-20 grid grid-cols-1 lg:grid-cols-[1fr_1fr] gap-8 lg:gap-0">
+    <div className="mx-12 mt-12 flex min-h-[504px] flex-col justify-start sm:mx-16 sm:mt-16 lg:mx-24 lg:mt-[96px]">
+      <div className="my-8 grid grid-cols-1 gap-8 sm:my-12 lg:my-20 lg:grid-cols-[1fr_1fr] lg:gap-0">
         <div className="max-w-full lg:max-w-[500px]">
-          <h1 className="mb-6 lg:mb-10 font-display text-4xl sm:text-5xl lg:text-6xl leading-tight lg:leading-none">5 reasons to choose Base</h1>
+          <h1 className="mb-6 font-display text-4xl leading-tight sm:text-5xl lg:mb-10 lg:text-6xl lg:leading-none">
+            5 reasons to choose Base
+          </h1>
           <span className="text-lg sm:text-xl lg:text-2xl">
             Five reasons why choosing Base will help you unleash onchain innovation and supercharge
             your project
@@ -39,32 +41,3 @@ export default async function Hero() {
     </div>
   );
 }
-
-async function TableOfContents({ sections }: TableOfContentsProps) {
-  return (
-    <div className="flex flex-col justify-center gap-2">
-      <ol className="list-none p-0">
-        {sections.map((section, index) => (
-          <div key={section.id} className="flex flex-col justify-end">
-            <li className="flex items-center text-lg sm:text-xl lg:text-2xl">
-              <span className="mr-3 lg:mr-4 flex h-6 w-6 sm:h-7 sm:w-7 lg:h-8 lg:w-8 flex-shrink-0 items-center justify-center rounded-full bg-white font-bold text-black">
-                {index + 1}
-              </span>
-              <Link href={`#${section.id}`}>{section.title}</Link>
-            </li>
-            {index < sections.length - 1 && <hr className="my-3 lg:my-4 w-[300px] sm:w-[350px] lg:w-[450px]" />}
-          </div>
-        ))}
-      </ol>
-    </div>
-  );
-}
-
-type TableOfContentsProps = {
-  sections: Section[];
-};
-
-type Section = {
-  title: string;
-  id: string;
-};
