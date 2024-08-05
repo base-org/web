68e21f9a96a915cd804fb9d7e2b318cde3b99402
Léo Galley <contact@kirkas.ch>
2024-08-05 15:02:21 -0400

    safe url parsing

--- apps/web/src/utils/images.ts
+++ apps/web/src/utils/images.ts
@@ -2,7 +2,7 @@ import { StaticImageData } from 'next/image';
 
 export const STATIC_IMAGE_FOLDER = '/_next/static/';
 export const PUBLIC_IMAGE_FOLDER = '/images/';
-export const VERCEL_BLOB_HOSTNAME = 'https://zku9gdedgba48lmr.public.blob.vercel-storage.com';
+export const VERCEL_BLOB_HOSTNAME = 'zku9gdedgba48lmr.public.blob.vercel-storage.com';
 
 export const getImageAbsoluteSource = (src: string | StaticImageData): string => {
   return typeof src === 'string' ? src : src.src;
@@ -20,7 +20,8 @@ export const shouldUseNextImage = (src: string | StaticImageData): boolean => {
   if (isPublicImage) return true;
 
   // Vercel blolb images (Basename Avatar)
-  const isVercelBlobImage = absoluteImageSource.startsWith(VERCEL_BLOB_HOSTNAME);
+  const url = new URL(absoluteImageSource);
+  const isVercelBlobImage = url.protocol === 'https:' && url.hostname === VERCEL_BLOB_HOSTNAME;
   if (isVercelBlobImage) return true;
 
   // Any other image, don't load via base.org / nextjs image proxy
