739dc01504e10babeb9a7af45ebd04a822bcb826
Brendan Forster <brendan.forster@coinbase.com>
2024-08-06 16:59:10 -0700

    added answer options as buttons

--- apps/web/app/(base-org)/surveys/[id]/page.tsx
+++ apps/web/app/(base-org)/surveys/[id]/page.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-
-import { getAllQuestions, getSurvey } from '../../../../src/apis/frameSurveys';
+import { getAllQuestions, getSurvey } from 'apps/web/src/apis/frameSurveys';
+import { ButtonWithSurveyResponse } from 'apps/web/src/components/Button/ButtonWithSurveyResponse';
 
 export async function generateStaticParams() {
   const questions = await getAllQuestions();
@@ -15,21 +14,23 @@ type SurveyProps = {
 
 export default async function Survey({ params }: SurveyProps) {
   const survey = await getSurvey(params.id);
-  console.log({ survey });
 
-  if (!survey) {
+  if (!survey || survey.answers.length === 0) {
     return <div>No Survey with this ID</div>;
   }
 
   return (
-    <div>
-      <span>Survey</span>
-      <p>Question: {survey.question.description ?? ''}</p>
-      <ol>
-        {survey.answers.map((answer) => (
-          <li key={answer.id}>{answer.description}</li>
-        ))}
-      </ol>
+    <div className="mt-[-96px] bg-blue-60">
+      <div className="mt-[96px]">
+        <p>Question: {survey.question.description ?? ''}</p>
+        <ol>
+          {survey.answers.map((answer) => (
+            <li key={answer.id}>
+              <ButtonWithSurveyResponse text={answer.description} surveyResponseId={answer.id} />
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 }
