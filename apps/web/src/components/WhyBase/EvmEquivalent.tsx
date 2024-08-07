3d1b9a9bdceb7b1865fbf13a7f9b896b6845b039
Brendan Forster <brendan.forster@coinbase.com>
2024-08-07 11:10:48 -0700

    fixed spacing and number icon

--- apps/web/src/components/WhyBase/EvmEquivalent.tsx
+++ apps/web/src/components/WhyBase/EvmEquivalent.tsx
@@ -2,7 +2,7 @@ import Image from 'apps/web/node_modules/next/image';
 import { ButtonWithLinkAndEventLogging } from '../Button/ButtonWithLinkAndEventLogging';
 import evmCompatibleChains from './images/EVM-compatibility-chains.png';
 
-const EVM_COMPATIBILITY_SECTION_ID = 'evmCompatibility';
+export const EVM_COMPATIBILITY_SECTION_ID = 'evmCompatibility';
 
 export default async function EvmEquivalent() {
   return (
@@ -13,20 +13,22 @@ export default async function EvmEquivalent() {
       <div className="hidden h-auto w-full bg-contain bg-center bg-no-repeat sm:my-[-25px] sm:block lg:my-[-40px]">
         <Image src={evmCompatibleChains} alt="EVM compatible chains" />
       </div>
-      <div className="flex w-full flex-col justify-center gap-8 sm:gap-12 lg:gap-16 xl:max-w-[550px]">
-        <h2 className="flex flex-row font-display text-3xl sm:text-4xl lg:text-6xl">
-          <span>3.</span>
-          <span className="ml-4">EVM Compatible for effortless migration</span>
-        </h2>
-        <div className="h-[235px] w-[250px] self-center bg-contain bg-center bg-no-repeat sm:hidden">
-          <Image src={evmCompatibleChains} alt="EVM compatible chains" />
-        </div>
-        <div>
-          <span className="text-base sm:text-lg">
-            Move your project to Base in minutes from any other EVM-compatible chain and unlock the
-            full potential of the Superchain and OP stack
+      <div className="flex w-full flex-row">
+        <div className="flex flex-row">
+          <span className="mt-1 flex h-6 w-6 flex-shrink-0 items-center justify-center rounded-full bg-white text-base font-bold text-black sm:h-7 sm:w-7 sm:text-xl lg:mr-4 lg:mt-2 lg:h-8 lg:w-8 lg:text-2xl">
+            3
           </span>
-          <div>
+          <div className="ml-4">
+            <h2 className="text-3xl sm:text-4xl lg:text-6xl">
+              EVM Compatible for effortless migration
+            </h2>
+            <div className="mb-8 mt-4 h-[235px] w-[250px] self-center bg-contain bg-center bg-no-repeat sm:hidden">
+              <Image src={evmCompatibleChains} alt="EVM compatible chains" />
+            </div>
+            <p className="my-2 text-base sm:text-lg lg:my-4 xl:my-6">
+              Move your project to Base in minutes from any other EVM-compatible chain and unlock
+              the full potential of the Superchain and OP stack
+            </p>
             <ButtonWithLinkAndEventLogging
               href="https://docs.base.org/docs"
               eventName="evm_compatible_start_migrating"
