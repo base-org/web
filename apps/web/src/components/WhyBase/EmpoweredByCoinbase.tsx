e84eb40ced11485057b51df18fbe2b9c7f54b3fd
Brendan Forster <brendan.forster@coinbase.com>
2024-08-07 11:11:26 -0700

    fixed spacing and number icon

--- apps/web/src/components/WhyBase/EmpoweredByCoinbase.tsx
+++ apps/web/src/components/WhyBase/EmpoweredByCoinbase.tsx
@@ -2,7 +2,7 @@ import Image from 'apps/web/node_modules/next/image';
 import { ButtonWithLinkAndEventLogging } from '../Button/ButtonWithLinkAndEventLogging';
 import empoweredByCoinbase from './images/EmpoweredByCoinbase.png';
 
-const EMPOWERED_BY_COINBASE_SECTION_ID = 'empoweredByCoinbase';
+export const EMPOWERED_BY_COINBASE_SECTION_ID = 'empoweredByCoinbase';
 
 export default async function EmpoweredByCoinbase() {
   return (
@@ -11,20 +11,20 @@ export default async function EmpoweredByCoinbase() {
       className="mb-6 mt-6 flex w-full max-w-[1440px] flex-col gap-8 px-12 sm:mb-8 sm:mt-8 sm:grid sm:grid-cols-[1fr_1fr] sm:gap-16 sm:px-16 lg:mb-10 lg:mt-10 lg:px-24"
     >
       <div className="flex w-full flex-col justify-center gap-8 lg:gap-16 xl:max-w-[550px]">
-        <h2 className="flex flex-row font-display text-3xl sm:text-4xl lg:text-6xl">
-          <span>5.</span>
-          <span className="ml-4">Empowered by Coinbase</span>
-        </h2>
-        <div className="h-[235px] w-[300px] self-center bg-contain bg-center bg-no-repeat sm:hidden">
-          <Image src={empoweredByCoinbase} alt="Empowered by Coinbase" />
-        </div>
-        <div>
-          <span className="text-base sm:text-lg">
-            {`Base is incubated within Coinbase and plans to progressively decentralize in the years
-            ahead. Leverage Coinbase's developer tools to make building easy and to reach Coinbase
-            users.`}
+        <div className="flex flex-row">
+          <span className="mt-1 flex h-6 w-6 flex-shrink-0 items-center justify-center rounded-full bg-white text-base font-bold text-black sm:h-7 sm:w-7 sm:text-xl lg:mr-4 lg:mt-2 lg:h-8 lg:w-8 lg:text-2xl">
+            5
           </span>
-          <div>
+          <div className="ml-4">
+            <h2 className="text-3xl sm:text-4xl lg:text-6xl">Empowered by Coinbase</h2>
+            <div className="my-4 h-auto w-full max-w-[300px] self-center bg-contain bg-center bg-no-repeat sm:hidden">
+              <Image src={empoweredByCoinbase} alt="Empowered by Coinbase" />
+            </div>
+            <p className="my-2 text-base sm:text-lg lg:my-4 xl:my-6">
+              {`Base is incubated within Coinbase and plans to progressively decentralize in the years
+              ahead. Leverage Coinbase's developer tools to make building easy and to reach Coinbase
+              users.`}
+            </p>
             <ButtonWithLinkAndEventLogging
               href="https://www.coinbase.com/blog/introducing-base"
               eventName="empowered_coinbase_learn_more"
