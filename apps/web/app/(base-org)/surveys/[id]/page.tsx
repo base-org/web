5b3ccf2a4b45a7c7ce44138568fb04da796adc1d
Brendan Forster <brendan.forster@coinbase.com>
2024-08-09 17:00:36 -0700

    simplified server-side component to simply delivery content to client-side

--- apps/web/app/(base-org)/surveys/[id]/page.tsx
+++ apps/web/app/(base-org)/surveys/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { getAllQuestions, getSurvey } from 'apps/web/src/apis/frameSurveys';
-import { ButtonWithSurveyResponse } from 'apps/web/src/components/Button/ButtonWithSurveyResponse';
+import SurveyContent from 'apps/web/src/components/Surveys/SurveyContent';
 
 export async function generateStaticParams() {
   const questions = await getAllQuestions();
@@ -20,17 +20,6 @@ export default async function Survey({ params }: SurveyProps) {
   }
 
   return (
-    <div className="mt-[-96px] bg-blue-60">
-      <div className="mt-[96px]">
-        <p>Question: {survey.question.description ?? ''}</p>
-        <ol>
-          {survey.answers.map((answer) => (
-            <li key={answer.id}>
-              <ButtonWithSurveyResponse text={answer.description} surveyResponseId={answer.id} />
-            </li>
-          ))}
-        </ol>
-      </div>
-    </div>
+    <SurveyContent surveyData={survey}/>
   );
 }
