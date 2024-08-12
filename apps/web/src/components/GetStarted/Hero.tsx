342a5ee0ce05f49c37461d80ba2da0dc5cda2ddc
Brendan Forster <brendan.forster@coinbase.com>
2024-08-12 11:20:18 -0700

    updated hero spacing

--- apps/web/src/components/GetStarted/Hero.tsx
+++ apps/web/src/components/GetStarted/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import classNames from 'classnames';
 import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 import gtcBackground from './images/gtc-background.svg';
 import ocsTitle from './images/onchain-summer.svg';
@@ -7,13 +8,21 @@ export default async function Hero() {
   const backgroundStyles = {
     backgroundImage: `url('${gtcBackground.src}')`,
   };
+
+  const textContainerClasses = classNames(
+    'max-w-full lg:max-w-[450px]',
+    'mr-12 sm:mr-0',
+    'ml-12 sm:ml-16 lg:ml-24 2xl:ml-0',
+    'mt-12 sm:mt-16 lg:mt-24',
+    'mb-6',
+  );
   return (
     <div
-      className="mt-[-96px] flex flex-col justify-center bg-[#283145] pt-8 font-display text-white sm:flex-row sm:pt-20 lg:pt-40"
+      className="mt-[-96px] flex w-full flex-col justify-center bg-[#283145] pt-8 font-display text-white sm:flex-row sm:pt-20 lg:pt-40"
       style={backgroundStyles}
     >
-      <div className="mx-12 mb-8 mt-12 flex flex-col justify-start sm:mx-16 sm:mb-12 sm:mt-16 lg:mx-24 max-w-[1440px]">
-        <div className="max-w-full lg:max-w-[450px]">
+      <div className="flex w-full max-w-[1440px] flex-col justify-between sm:flex-row">
+        <div className={textContainerClasses}>
           <h1 className="mb-6 font-display text-4xl leading-tight sm:text-5xl lg:mb-10 lg:text-[80px] lg:leading-none">
             Resources for Builders
           </h1>
@@ -21,9 +30,9 @@ export default async function Hero() {
             Get help to build and grow your project on Base with our Go-to-Community Kit
           </span>
         </div>
-      </div>
-      <div className="self-end">
-        <Image src={ocsTitle as StaticImport} alt="onchain summer" />
+        <div className="self-end">
+          <Image src={ocsTitle as StaticImport} alt="onchain summer" />
+        </div>
       </div>
     </div>
   );
