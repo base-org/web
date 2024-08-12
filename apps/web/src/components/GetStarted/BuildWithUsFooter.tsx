2b4d47f1ae0ebc33c75a0caadc8aad86833de8d0
Brendan Forster <brendan.forster@coinbase.com>
2024-08-12 11:18:55 -0700

    rounded buttons

--- apps/web/src/components/GetStarted/BuildWithUsFooter.tsx
+++ apps/web/src/components/GetStarted/BuildWithUsFooter.tsx
@@ -14,13 +14,14 @@ export default async function BuildWithUsFooter() {
         style={imageBackgroundStyles}
       >
         <h2 className={`${titleClasses} mb-6 text-center sm:mb-8`}>Start building with us</h2>
-        <div className="flex w-full flex-col items-center justify-center sm:w-auto sm:flex-row sm:gap-8">
+        <div className="flex w-full flex-col items-center justify-center gap-4 sm:w-auto sm:flex-row sm:gap-8">
           <ButtonWithLinkAndEventLogging
             href="https://lu.ma/base-officehours/?utm_source=dotorg&medium=builderkit"
             eventName="start_building_with_us_contact_us"
             target="_blank"
             rel="noreferrer noopener"
-            buttonClassNames={linkTextClasses}
+            // linkClassNames='w-full'
+            buttonClassNames={`${linkTextClasses} rounded-[3px]`}
           >
             Office Hours
           </ButtonWithLinkAndEventLogging>
@@ -30,7 +31,7 @@ export default async function BuildWithUsFooter() {
             target="_blank"
             rel="noreferrer noopener"
             variant={ButtonVariants.Secondary}
-            buttonClassNames={linkTextClasses}
+            buttonClassNames={`${linkTextClasses} rounded-[3px]`}
           >
             View Our Docs
           </ButtonWithLinkAndEventLogging>
