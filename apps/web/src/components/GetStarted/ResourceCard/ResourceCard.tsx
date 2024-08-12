89d0293cfedd177be8d7d330ccdce7098769e208
Brendan Forster <brendan.forster@coinbase.com>
2024-08-12 15:01:51 -0700

    refactored and renamed ResourceCard, deleted ResourceGrid

--- apps/web/src/components/GetStarted/ResourceCard/ResourceCard.tsx
+++ apps/web/src/components/GetStarted/ResourceCard/ResourceCard.tsx
@@ -2,7 +2,6 @@
 
 import { useCallback } from 'react';
 import Link from 'next/link';
-import classNames from 'classnames';
 import { ActionType, ComponentType } from 'base-ui/utils/logEvent';
 import { useAnalytics } from '../../../../contexts/Analytics';
 import { ResourceCardProps } from '../resourceTypes';
@@ -11,20 +10,18 @@ export default function ResourceCard({
   href,
   title,
   description,
-  topLeft = <span />,
-  topRight = <span />,
-  colorOne,
-  colorTwo,
+  topLeft = null,
+  topRight = null,
+  classnames
 }: ResourceCardProps) {
   const { logEventWithContext } = useAnalytics();
 
-  const resourceCardWrapperStyle = classNames(
-    'group border-2 p-7 text-white',
-    `odd:bg-${colorOne} odd:border-${colorOne}`,
-    `even:bg-${colorTwo ?? colorOne} even:border-${colorTwo ?? colorOne}`,
-    'duration-200',
-    'hover:scale-105 hover:shadow-lg hover:shadow-gray-80',
-  );
+  const resourceCardWrapperStyle = `
+    group border-2 p-7 text-white
+    duration-200
+    hover:scale-105 hover:shadow-lg hover:shadow-gray-80
+    ${classnames}
+  `;
 
   const handleClick = useCallback(() => {
     logEventWithContext(title.replace(/ /g, '_').replace(/\W/g, ''), ActionType.click, {
@@ -36,8 +33,8 @@ export default function ResourceCard({
     <div key={href} className={resourceCardWrapperStyle}>
       <Link href={href} onClick={handleClick}>
         <div className="flex w-full flex-row justify-between">
-          <div>{topLeft}</div>
-          <div>{topRight}</div>
+          {topLeft}
+          {topRight}
         </div>
         <div className="mt-8">
           <h3 className="mb-4 font-mono text-lg uppercase sm:text-xl">{title}</h3>
