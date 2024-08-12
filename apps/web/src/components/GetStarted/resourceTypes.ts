25613a485b7895f5229c56ee1fbdb941cf2c8b68
Brendan Forster <brendan.forster@coinbase.com>
2024-08-12 15:05:28 -0700

    removed unused types

--- apps/web/src/components/GetStarted/resourceTypes.ts
+++ apps/web/src/components/GetStarted/resourceTypes.ts
@@ -5,21 +5,9 @@ export type Resource = {
   icon?: string;
 };
 
-export type ResourceSectionType = {
-  title: string;
-  colorOne: string;
-  colorTwo: string;
-  cards: Resource[];
-};
-
-export type ResourceGridProps = {
-  section: ResourceSectionType;
-};
-
 export type ResourceCardProps = Resource & {
   counter?: number;
   topLeft?: React.ReactNode;
   topRight?: React.ReactNode;
-  colorOne: string;
-  colorTwo?: string;
+  classnames?: string;
 };
