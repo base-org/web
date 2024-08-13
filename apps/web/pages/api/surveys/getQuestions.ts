13a511dcf5d094da84cc0bd0247c26a32559fb04
Brendan Forster <brendan.forster@coinbase.com>
2024-08-13 15:47:56 -0700

    commented out files for probable future deletion

--- apps/web/pages/api/surveys/getQuestions.ts
+++ apps/web/pages/api/surveys/getQuestions.ts
@@ -1,19 +1,19 @@
-import { NextApiRequest, NextApiResponse } from 'next/dist/shared/lib/utils';
+// import { NextApiRequest, NextApiResponse } from 'next/dist/shared/lib/utils';
 
-import { surveyDb } from '../../../src/apis/frameSurveys';
+// import { surveyDb } from '../../../src/apis/frameSurveys';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log({ surveyDb });
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   console.log({ surveyDb });
 
-  if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Method Not Allowed' });
-  }
+//   if (req.method !== 'GET') {
+//     return res.status(405).json({ error: 'Method Not Allowed' });
+//   }
 
-  try {
-    const questions = await surveyDb.selectFrom('frame_survey_questions').selectAll().execute();
-    return res.status(200).json(questions);
-  } catch (error) {
-    console.error('Failed to fetch questions:', error);
-    return res.status(500).json({ error: 'Internal Server Error' });
-  }
-}
+//   try {
+//     const questions = await surveyDb.selectFrom('frame_survey_questions').selectAll().execute();
+//     return res.status(200).json(questions);
+//   } catch (error) {
+//     console.error('Failed to fetch questions:', error);
+//     return res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// }
