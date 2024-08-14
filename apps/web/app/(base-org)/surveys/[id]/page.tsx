a720a26bbf2c4e1e2c3a91085a36bc3a3629d295
Brendan Forster <brendan.forster@coinbase.com>
2024-08-14 13:27:52 -0700

    metadata updates to support frames

--- apps/web/app/(base-org)/surveys/[id]/page.tsx
+++ apps/web/app/(base-org)/surveys/[id]/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from 'next';
+import {
+  getFrameMetadata,
+  FrameMetadataType,
+  FrameMetadataResponse,
+} from '@coinbase/onchainkit/frame';
 import { getAllQuestions, getSurvey } from '../../../../src/apis/frameSurveys';
 import SurveyContent from '../../../../src/components/Surveys/SurveyContent';
 
@@ -16,6 +22,32 @@ export async function generateStaticParams() {
 type SurveyProps = {
   params: { id: number };
 };
+
+export async function generateMetadata({ params }: SurveyProps): Promise<Metadata> {
+  const survey = await getSurvey(params.id);
+  const frameMetadata = {
+    buttons: survey?.answers.map((answer) => ({
+      action: 'post',
+      label: answer.description,
+      target: `http://localhost:3000/api/surveys/postUserResponse?question_id=${survey.question.id}&answer_id=${answer.id}`,
+    })),
+    image: 'https://base.org/images/base-open-graph.png',
+    postUrl: 'http://localhost:3000/api/surveys/postUserResponseXXXXXXXXXX',
+  };
+
+  return {
+    title: 'Based Surveys',
+    description: 'LFG',
+    openGraph: {
+      title: 'Base.org | Surveys',
+      description: 'LFGooooo',
+      images: ['https://base.org/images/base-open-graph.png'],
+    },
+    other: {
+      ...getFrameMetadata(frameMetadata),
+    },
+  };
+}
 
 export default async function Survey({ params }: SurveyProps) {
   const survey = await getSurvey(params.id);
