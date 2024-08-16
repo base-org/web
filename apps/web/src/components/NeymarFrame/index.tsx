4addf793197135eee1dc727633a1678a85b002fa
Léo Galley <contact@kirkas.ch>
2024-08-16 13:18:13 -0400

    cleanup

--- apps/web/src/components/NeymarFrame/index.tsx
+++ apps/web/src/components/NeymarFrame/index.tsx
@@ -1,7 +1,8 @@
 import ImageWithLoading from 'apps/web/src/components/ImageWithLoading';
 import { NeynarFrame } from 'apps/web/src/utils/frames';
 
-// Simple frame, without buttons
+// Frame displayed from Neymar API data
+// No buttons or interactions for now, just a link to the frame source
 export default function NeymarFrame({ frame }: { hash: string; frame: NeynarFrame }) {
   return (
     <div className="overflow-hidden rounded-3xl border border-gray-40/20">
