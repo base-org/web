38cad470b1141349abfcf6c2b70ba108b2b7c718
Brendan Forster <brendan.forster@coinbase.com>
2024-08-19 09:02:40 -0700

    converted animations to pure css via tailwind

--- apps/web/src/components/Basenames/RegistrationLandingExplore/index.tsx
+++ apps/web/src/components/Basenames/RegistrationLandingExplore/index.tsx
@@ -1,25 +1,6 @@
-import { motion } from 'framer-motion';
 import classNames from 'classnames';
 import { useRegistration } from 'apps/web/src/components/Basenames/RegistrationContext';
 import { Icon } from 'apps/web/src/components/Icon/Icon';
-
-const divAnimation = {
-  scale: [0.95, 0.98, 1, 1, 0.98, 0.95],
-};
-
-const animate = {
-  opacity: [0, 0.1, 1, 1, 0, 0],
-  y: ['-75%', '-50%', '-5%', '5%', '100%', '100%'],
-};
-
-const transition = {
-  duration: 2,
-  delay: 0.5,
-  repeat: Infinity,
-  repeatType: 'loop',
-  ease: 'linear',
-  times: [0, 0.1, 0.3, 0.7, 0.9, 1],
-};
 
 export default function RegistrationLandingExplore({}) {
   const { searchInputFocused } = useRegistration();
@@ -35,13 +16,11 @@ export default function RegistrationLandingExplore({}) {
         {' '}
         Scroll to explore
       </span>
-      <motion.div animate={divAnimation} transition={transition}>
-        <div className="h-[25px] w-[25px] rounded-lg bg-[#e7e6e2] py-1.5 pl-[6px]">
-          <motion.div animate={animate} transition={transition}>
-            <Icon name="caret" color="black" width="12" height="12" />
-          </motion.div>
+      <div className="h-[25px] w-[25px] animate-pulsate rounded-lg bg-[#e7e6e2] py-1.5 pl-[6px]">
+        <div className="animate-verticalSlide">
+          <Icon name="caret" color="black" width="12" height="12" />
         </div>
-      </motion.div>
+      </div>
     </div>
   );
 }
