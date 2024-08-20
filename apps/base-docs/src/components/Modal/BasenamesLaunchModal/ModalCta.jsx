0471c10045b99409a7e9bbf7c5615a9a129a9f4b
Brendan from DeFi <brendan.forster@coinbase.com>
2024-08-20 12:58:10 -0700

    Feat: Basename Launch Modal on Docs (#883)
    * added Analytics context to libs
    
    * added basenames modal image
    
    * created basenames launch modal
    
    * allow dynamic color for close icon
    
    * add modal to docs homepage
    
    * fixed z-index issue
    
    * fixed banner icon color

---
+++ apps/base-docs/src/components/Modal/BasenamesLaunchModal/ModalCta.jsx
@@ -0,0 +1,21 @@
+import React, { useCallback } from 'react';
+import Link from '@docusaurus/Link';
+import { useAnalytics } from 'base-ui/contexts/Analytics';
+import { ActionType, ComponentType } from 'base-ui/utils/logEvent';
+
+export default function ModalCta({ buttonClassNames, setIsModalOpen }) {
+  const { logEventWithContext } = useAnalytics();
+
+  const handleClick = useCallback(() => {
+    logEventWithContext('get_a_basename', ActionType.click, {
+      componentType: ComponentType.button,
+    });
+    setIsModalOpen(false);
+  }, [logEventWithContext, setIsModalOpen]);
+
+  return (
+    <Link href="/names" onClick={handleClick}>
+      <button className={buttonClassNames}>Get your basename</button>
+    </Link>
+  );
+}
