fd89a5b16813ff4a912935af553c6f0f8c04ad40
Brendan Forster <brendan.forster@coinbase.com>
2024-08-21 16:53:37 -0700

    updated txSuccess logic

--- apps/web/pages/api/basenames/frame/txSuccess.ts
+++ apps/web/pages/api/basenames/frame/txSuccess.ts
@@ -2,27 +2,43 @@ import { NextApiRequest, NextApiResponse } from 'next/dist/shared/lib/utils';
 import { FrameRequest, getFrameMessage } from '@coinbase/onchainkit/frame';
 
 import { txSuccessFrame } from 'apps/web/pages/api/basenames/frame/frameResponses';
+import type { TxFrameStateType } from 'apps/web/pages/api/basenames/frame/tx';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: `TxSuccess Screen — Method (${req.method}) Not Allowed` });
+  }
+
+  // const url = new URL(req.url);
+  // const name = url.searchParams.get('name') ?? '';
+
   const body = req.body as FrameRequest;
   let message;
   let isValid;
+  let name;
 
   try {
     const result = await getFrameMessage(body, {
-      neynarApiKey: 'BF56615F-9028-4774-9E8C-2745308382C1',
+      neynarApiKey: 'BF56615F-9028-4774-9E8C-2745308382C1', // TODO DON"T HARDCODE THIS
     });
     isValid = result.isValid;
     message = result.message;
-    if (!isValid || !message) {
+    if (!isValid) {
       throw new Error('Message is not valid');
     }
+    if (!message) {
+      throw new Error('No message received');
+    }
+
+    const messageState = JSON.parse(
+      decodeURIComponent(message.state?.serialized),
+    ) as TxFrameStateType;
+    if (!messageState) {
+      throw new Error('No message state received');
+    }
+    name = messageState.targetName;
+    return res.status(200).setHeader('Content-Type', 'text/html').send(txSuccessFrame(name));
   } catch (e) {
     return res.status(500).json({ error: e });
   }
-
-  const { untrustedData } = body;
-  const txId: string = untrustedData.transactionId ?? '--';
-
-  return res.status(200).setHeader('Content-Type', 'text/html').send(txSuccessFrame(txId));
 }
