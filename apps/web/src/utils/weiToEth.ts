10baaa92b7b887bf516daa5899e34592e161411d
Brendan Forster <brendan.forster@coinbase.com>
2024-08-22 13:55:53 -0700

    better variable name

--- apps/web/src/utils/weiToEth.ts
+++ apps/web/src/utils/weiToEth.ts
@@ -4,10 +4,10 @@ export function weiToEth(wei?: bigint): number | '...' {
   if (wei === undefined) {
     return '...';
   }
-  const value = parseFloat(formatEther(wei));
-  if (value < 0.001) {
-    return parseFloat(value.toFixed(4));
+  const eth = parseFloat(formatEther(wei));
+  if (eth < 0.001) {
+    return parseFloat(eth.toFixed(4));
   } else {
-    return parseFloat(value.toFixed(3));
+    return parseFloat(eth.toFixed(3));
   }
 }
