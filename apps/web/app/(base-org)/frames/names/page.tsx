1b8a2c5c6ecabe910640e200f76fa96e7c484f68
Brendan Forster <brendan.forster@coinbase.com>
2024-08-21 20:36:11 -0700

    reworked placeholder landing page

--- apps/web/app/(base-org)/frames/names/page.tsx
+++ apps/web/app/(base-org)/frames/names/page.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
-import basenameCover from 'apps/web/app/(basenames)/names/basename_cover.png';
+import Image from 'apps/web/node_modules/next/image';
+import Link from 'apps/web/node_modules/next/link';
+import claimBasename from 'apps/web/public/images/frames/basenames/initial-image.png';
 import { initialFrame } from 'apps/web/pages/api/basenames/frame/frameResponses';
 
 export const metadata: Metadata = {
@@ -10,7 +12,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: `Basenames | Frame`,
     url: `/frames/names`,
-    images: [basenameCover.src],
+    images: [claimBasename.src],
   },
   twitter: {
     site: '@base',
@@ -22,5 +24,15 @@ export const metadata: Metadata = {
 };
 
 export default async function NameFrame() {
-  return <div>Hello Neo</div>;
+  return (
+    <div className="mt-[-96px] flex w-full flex-col items-center bg-black pb-[96px]">
+      <div className="flex h-screen w-full max-w-[1440px] flex-col items-center justify-center gap-12 px-8 py-8 pt-28">
+        <div className="flex aspect-[993/516] h-auto w-full max-w-[1024px] flex-col items-center relative">
+          <Link href='/names'>
+            <Image src={claimBasename.src} alt="Claim a basename today" fill />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
