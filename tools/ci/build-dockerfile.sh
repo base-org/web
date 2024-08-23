6033b37e12bbe4b68229c5971a0b9842daac53bc
David Roth <david.roth@coinbase.com>
2024-08-23 14:17:05 -0700

    Revert "Build with amd64 compatible image (#925)" (#930)
    This reverts commit f1f91267bba45b362c574445f766055b9387a70e.

--- tools/ci/build-dockerfile.sh
+++ tools/ci/build-dockerfile.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 # We have to put this in a separate file as the pipeline
 # command needs to be static, and this env var breaks that.
-docker build --platform=linux/arm64 --pull -f "$DOCKERFILE_PATH" -t ci_build_test_image --build-arg CODEFLOW_COMMIT_TAG .
+docker build --pull -f "$DOCKERFILE_PATH" -t ci_build_test_image --build-arg CODEFLOW_COMMIT_TAG .
 
 # Cleanup after a successful build to avoid the agent
 # running out of disk space!
