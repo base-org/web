d4758cde26fbb71c5cc801f2a39c7152a5203cf5
David Roth <david.roth@coinbase.com>
2024-08-26 16:45:32 -0700

    Test with fixed hostname

--- apps/web/tracer.ts
+++ apps/web/tracer.ts
@@ -1,13 +1,31 @@
 import ddtracer from 'dd-trace';
+import { logger } from 'apps/web/src/utils/logger';
 
 const { tracer } = ddtracer;
 tracer.init({
   service: process.env.CODEFLOW_PROJECT_NAME,
-}); // initialized in a different file to avoid hoisting.
+  // Enables tracking performance trends across versions. Using commit hash as
+  // code changes most often result in performance changes (vs deploys).
+  //
+  // Codeflow Docs: https://confluence.coinbase-corp.com/pages/viewpage.action?pageId=1172681623
+  // Datadog Docs: https://datadoghq.dev/dd-trace-js/
+  version: process.env.CODEFLOW_COMMIT_HASH,
+  profiling: true,
+  logInjection: true,
+  startupLogs: true,
+  logger: {
+    error: (err) => logger.error(err),
+    warn: (message) => logger.warn(message),
+    info: (message) => logger.info(message),
+    debug: (message) => logger.debug(message),
+  },
+  dogstatsd: {
+    hostname: '10.166.21.89',
+  },
+});
 
 tracer.use('http', {
-  service: process.env.CODEFLOW_SERVICE_NAME,
   enabled: true,
 });
-// export declare const tracer: Tracer;
+
 export default tracer;
