51986442fac73a041ff6f80bb7023096787f950b
Matthew Bunday <matthew.bunday@coinbase.com>
2024-08-27 14:11:57 -0400

    Add buildathon SBT winner badge

--- apps/web/src/components/Basenames/UsernameProfileSectionBadges/hooks/useBuildathon.ts
+++ apps/web/src/components/Basenames/UsernameProfileSectionBadges/hooks/useBuildathon.ts
@@ -3,7 +3,10 @@ import BuildathonSBT from 'apps/web/src/abis/BuildathonSBT';
 
 const PARTICIPANT_SBT_ADDRESS = '0x59ca61566C03a7Fb8e4280d97bFA2e8e691DA3a6';
 
-export default function useBuildathonParticipant(address?: `0x${string}`): boolean {
+export default function useBuildathonParticipant(address?: `0x${string}`): {
+  isParticipant: boolean;
+  isWinner: boolean;
+} {
   const { data: balanceOf } = useReadContract({
     address: PARTICIPANT_SBT_ADDRESS,
     abi: BuildathonSBT,
@@ -14,6 +17,7 @@ export default function useBuildathonParticipant(address?: `0x${string}`): boole
     },
   });
 
-  if (!balanceOf) return false;
-  return balanceOf > 0;
+  if (!balanceOf) return { isParticipant: false, isWinner: false };
+  // winners will have both a particpant SBT and a winner SBT
+  return { isParticipant: balanceOf > 0, isWinner: balanceOf > 1 };
 }
