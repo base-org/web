e83738914d2e803ecafb808b7b829a4491733d76
Léo Galley <contact@kirkas.ch>
2024-08-27 18:08:48 -0400

    Lottie animations for registration background (1/3) and Basename default avatar (#951)
    * Relace vortex.web with vortex.json, add Lottie integration
    
    * cleanup
    
    * fix import shenanigans

---
+++ apps/web/src/components/LottieAnimation/index.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+
+// Airbnb lottie.
+// Pros: Light, easy to use, doesn't require a WASM binary or CDN proxy
+// Cons: Doesn't support custom .lottie files, only JSON
+import lottie, { AnimationItem } from 'lottie-web';
+
+export default function LottieAnimation({
+  data,
+  wrapperClassName,
+}: {
+  data: unknown;
+  wrapperClassName?: string;
+}) {
+  const animationContainer = useRef<HTMLDivElement>(null);
+  const lotteAnimation = useRef<AnimationItem>();
+
+  useEffect(() => {
+    if (!animationContainer.current) return;
+    if (lotteAnimation.current) return;
+    lotteAnimation.current = lottie.loadAnimation({
+      container: animationContainer.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: data,
+      rendererSettings: {},
+    });
+  }, [data]);
+
+  return <div ref={animationContainer} className={wrapperClassName} />;
+}
