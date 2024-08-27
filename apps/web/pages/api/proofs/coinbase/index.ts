1502a1ba75798678d9c14c0de803f270f2fbc685
David Roth <david.roth@coinbase.com>
2024-08-27 12:46:56 -0700

    Fix build

--- apps/web/pages/api/proofs/coinbase/index.ts
+++ apps/web/pages/api/proofs/coinbase/index.ts
@@ -9,7 +9,7 @@ import {
 import { sybilResistantUsernameSigning } from 'apps/web/src/utils/proofs/sybil_resistance';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Address } from 'viem';
-import tracer from 'apps/web/dd-trace/initialize';
+import tracer from 'dd-trace';
 
 // Coinbase verified account *and* CB1 structure
 export type CoinbaseProofResponse = {
