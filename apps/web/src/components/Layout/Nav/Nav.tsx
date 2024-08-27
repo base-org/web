ffdbd8ced907b711835e2ba36a17585b4bae28c6
Léo Galley <contact@kirkas.ch>
2024-08-27 16:20:39 -0400

    Maybe will fix google thumnail (#950)

--- apps/web/src/components/Layout/Nav/Nav.tsx
+++ apps/web/src/components/Layout/Nav/Nav.tsx
@@ -4,9 +4,11 @@ import Link from 'next/link';
 import dynamic from 'next/dynamic';
 import { usePathname } from 'next/dist/client/components/navigation';
 import AnalyticsProvider from 'apps/web/contexts/Analytics';
-import { Logo } from '../../Logo/Logo';
 import DesktopNav from './DesktopNav';
 import MobileMenu from './MobileMenu';
+import logoBlack from './logoBlack.svg';
+import logoWhite from './logoWhite.svg';
+import Image, { StaticImageData } from 'next/image';
 
 const DynamicBanner = dynamic(async () => import('base-ui/components/Layout/Nav/Banner'), {
   ssr: false,
@@ -24,8 +26,9 @@ const BLACK_NAV_PATHS = [
 export default function Nav() {
   const pathname = usePathname();
   const color = pathname && BLACK_NAV_PATHS.includes(pathname) ? 'black' : 'white';
+  const logo = color === 'black' ? (logoBlack as StaticImageData) : (logoWhite as StaticImageData);
   return (
-    <AnalyticsProvider context='navbar'>
+    <AnalyticsProvider context="navbar">
       <DynamicBanner
         bannerName="basenamesLaunchBanner"
         href="https://base.org/names?utm_source=dotorg&utm_medium=banner"
@@ -33,7 +36,7 @@ export default function Nav() {
       />
       <nav className="z-10 flex h-24 w-full max-w-[1440px] flex-row items-center justify-between gap-16 self-center bg-transparent p-8">
         <Link href="/" aria-label="Base Homepage">
-          <Logo color={color} width="106px" />
+          <Image src={logo} alt="Base Logo" title="Base Logo" width="106" />
         </Link>
         <DesktopNav color={color} />
         <MobileMenu color={color} />
