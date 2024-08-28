1b126707f36c56ebcb5faa9f6e3db1ab915b3041
Brendan Forster <brendan.forster@coinbase.com>
2024-08-28 14:09:17 -0700

    neynar key for testing

--- apps/web/pages/api/basenames/frame/constants.ts
+++ apps/web/pages/api/basenames/frame/constants.ts
@@ -6,8 +6,9 @@ export const DOMAIN = isDevelopment
   ? `http://localhost:3000`
   : 'https://base-web-git-feat-frame-tx-submission-coinbase-vercel.vercel.app';
 
-export const NEYNAR_API_KEY = isDevelopment
-  ? 'BF56615F-9028-4774-9E8C-2745308382C1'
-  : process.env.NEXT_PUBLIC_NEYNAR_API_KEY;
+// export const NEYNAR_API_KEY = isDevelopment
+//   ? 'BF56615F-9028-4774-9E8C-2745308382C1'
+//   : process.env.NEXT_PUBLIC_NEYNAR_API_KEY;
+export const NEYNAR_API_KEY = 'BF56615F-9028-4774-9E8C-2745308382C1'; // for testing
 
 export const CHAIN = baseSepolia;
