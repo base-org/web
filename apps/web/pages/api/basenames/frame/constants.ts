058c098fda4063ddb120de70df079c892e52bbb4
Brendan Forster <brendan.forster@coinbase.com>
2024-08-28 12:33:06 -0700

    updated domain for testing

--- apps/web/pages/api/basenames/frame/constants.ts
+++ apps/web/pages/api/basenames/frame/constants.ts
@@ -1,4 +1,5 @@
 import { isDevelopment } from 'apps/web/src/constants';
 
-export const DOMAIN = isDevelopment ? `http://localhost:3000` : 'https://www.base.org';
+// export const DOMAIN = isDevelopment ? `http://localhost:3000` : 'https://www.base.org';
+export const DOMAIN = isDevelopment ? `http://localhost:3000` : 'https://base-web-git-feat-frame-tx-submission-coinbase-vercel.vercel.app';
 export const NEYNAR_API_KEY = process.env.NEXT_PUBLIC_NEYNAR_API_KEY;
