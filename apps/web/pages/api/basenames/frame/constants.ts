e7631bae19fa941ddb539f3cde391adee1f121d9
Brendan Forster <brendan.forster@coinbase.com>
2024-08-28 22:43:48 -0700

    removed comments from constants

--- apps/web/pages/api/basenames/frame/constants.ts
+++ apps/web/pages/api/basenames/frame/constants.ts
@@ -4,11 +4,4 @@ import { base } from 'viem/chains';
 export const DOMAIN = isDevelopment ? `http://localhost:3000` : 'https://www.base.org';
 export const NEYNAR_API_KEY = process.env.NEXT_PUBLIC_NEYNAR_API_KEY;
 
-/* FOR VERCEL PREVIEW TESTING
-export const DOMAIN = isDevelopment
-  ? `http://localhost:3000`
-  : 'VERCEL PREVIEW LINK';
-export const NEYNAR_API_KEY = 'BF56615F-9028-4774-9E8C-2745308382C1';
-*/
-
 export const CHAIN = base;
