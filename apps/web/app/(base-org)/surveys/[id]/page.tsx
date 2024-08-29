6c18cb4f879ec4ba1dbd234f6bd8b93f4a9bd867
Brendan Forster <brendan.forster@coinbase.com>
2024-08-29 16:20:46 -0700

    updated landing page to bare minimum SSR

--- apps/web/app/(base-org)/surveys/[id]/page.tsx
+++ apps/web/app/(base-org)/surveys/[id]/page.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from 'next';
 import { getFrameMetadata } from '@coinbase/onchainkit/frame';
 import { DOMAIN } from 'apps/web/src/constants';
-import { getAllQuestions, getSurvey } from '../../../../src/apis/frameSurveys';
-import SurveyContent from '../../../../src/components/Surveys/SurveyContent';
+import { getAllSurveys, getSurvey } from 'apps/web/src/apis/frameSurveys';
+import SurveyContent from 'apps/web/src/components/Surveys/SurveyContent';
 
 export async function generateStaticParams() {
   try {
-    const questions = await getAllQuestions();
-    return questions?.map((question) => ({
-      id: String(question.id),
+    const surveys = await getAllSurveys();
+    return surveys?.map((survey) => ({
+      id: String(survey.id),
     }));
   } catch (error) {
     console.error('Could not get questions:', error);
@@ -20,40 +20,50 @@ type SurveyProps = {
   params: { id: number };
 };
 
-export async function generateMetadata({ params }: SurveyProps): Promise<Metadata> {
-  const survey = await getSurvey(params.id);
-  const frameMetadata = {
-    buttons: survey?.answers.map((answer) => ({
-      action: 'post',
-      label: answer.description,
-      postUrl: `http://localhost:3000/api/surveys/frameUserResponse?questionId=${survey.question.id}&answerId=${answer.id}`,
-    })),
-    image: {
-      src: `${DOMAIN}/api/surveys/assets/questionFrameImage.png?survey=${survey?.question.description}`,
-    },
-    postUrl: 'http://localhost:3000/api/surveys/frameUserResponse',
-  };
+// export async function generateMetadata({ params }: SurveyProps): Promise<Metadata> {
+//   const survey = await getSurvey(params.id);
+//   const frameMetadata = {
+//     buttons: survey?.answers.map((answer) => ({
+//       action: 'post',
+//       label: answer.description,
+//       postUrl: `http://localhost:3000/api/surveys/frameUserResponse?questionId=${survey.question.id}&answerId=${answer.id}`,
+//     })),
+//     image: {
+//       src: `${DOMAIN}/api/surveys/assets/questionFrameImage.png?survey=${survey?.question.description}`,
+//     },
+//     postUrl: 'http://localhost:3000/api/surveys/frameUserResponse',
+//   };
 
-  return {
-    title: 'Based Surveys',
-    description: 'LFG',
-    openGraph: {
-      title: 'Base.org | Surveys',
-      description: 'LFGooooo',
-      images: ['https://base.org/images/base-open-graph.png'],
-    },
-    other: {
-      ...(getFrameMetadata(frameMetadata) as Record<string, string>),
-    },
-  };
-}
+//   return {
+//     title: 'Based Surveys',
+//     description: 'LFG',
+//     openGraph: {
+//       title: 'Base.org | Surveys',
+//       description: 'LFGooooo',
+//       images: ['https://base.org/images/base-open-graph.png'],
+//     },
+//     other: {
+//       ...(getFrameMetadata(frameMetadata) as Record<string, string>),
+//     },
+//   };
+// }
 
 export default async function Survey({ params }: SurveyProps) {
   const survey = await getSurvey(params.id);
 
-  if (!survey || survey.answers.length === 0) {
-    return <div>No Survey with this ID</div>;
-  }
+  return (
+    <div className="mt-[-96px] bg-black">
+      <div className="mt-[96px] flex h-screen flex-col items-center">
+        <div className="mt-40 w-[740px] bg-blue-60 text-center text-white">
+          {!survey ? (
+            <h1 className="my-10 text-3xl">No Survey with this ID</h1>
+          ) : (
+            <SurveyContent survey={survey} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 
-  return <SurveyContent surveyData={survey} />;
+  // return <SurveyContent surveyData={survey} />;
 }
