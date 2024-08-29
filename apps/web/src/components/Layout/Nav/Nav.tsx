235ea3801c818e8fa6cae2ba10ba7b39ca442744
Brendan Forster <brendan.forster@coinbase.com>
2024-08-29 10:27:29 -0700

    removed basenames banners from web and docs

--- apps/web/src/components/Layout/Nav/Nav.tsx
+++ apps/web/src/components/Layout/Nav/Nav.tsx
@@ -10,10 +10,6 @@ import logoBlack from './logoBlack.svg';
 import logoWhite from './logoWhite.svg';
 import Image, { StaticImageData } from 'next/image';
 
-const DynamicBanner = dynamic(async () => import('base-ui/components/Layout/Nav/Banner'), {
-  ssr: false,
-});
-
 const BLACK_NAV_PATHS = [
   '/',
   '/jobs/apply',
@@ -29,11 +25,6 @@ export default function Nav() {
   const logo = color === 'black' ? (logoBlack as StaticImageData) : (logoWhite as StaticImageData);
   return (
     <AnalyticsProvider context="navbar">
-      <DynamicBanner
-        bannerName="basenamesLaunchBanner"
-        href="https://base.org/names?utm_source=dotorg&utm_medium=banner"
-        text="Claim Your Basename Today!"
-      />
       <nav className="z-10 flex h-24 w-full max-w-[1440px] flex-row items-center justify-between gap-16 self-center bg-transparent p-8">
         <Link href="/" aria-label="Base Homepage">
           <Image src={logo} alt="Base Logo" title="Base Logo" width="106" />
