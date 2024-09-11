1a2189fe2806d7feab5b136dc1d88f8bb9ca8953
Léo Galley <contact@kirkas.ch>
2024-09-11 14:30:02 -0400

    adapt jobs page

--- apps/web/app/(base-org)/jobs/page.tsx
+++ apps/web/app/(base-org)/jobs/page.tsx
@@ -1,3 +1,4 @@
+import Container from 'apps/web/src/components/base-org/Container';
 import Title from 'apps/web/src/components/base-org/typography/Title';
 import { TitleLevel } from 'apps/web/src/components/base-org/typography/Title/types';
 import type { Metadata } from 'next';
@@ -16,13 +17,15 @@ export const metadata: Metadata = {
 
 export default async function Jobs() {
   return (
-    <main className="mt-[-96px] flex w-full grow flex-col items-center bg-black">
-      <section className="mb-[140px] mt-[100px] flex w-full max-w-[1440px] flex-col px-8 pb-10 sm:mt-[150px]">
-        <Title level={TitleLevel.Display3}>Join our team</Title>
-        <div className="flex flex-col font-display text-sm text-white lg:text-xl">
-          <JobsList />
-        </div>
-      </section>
+    <main className="flex w-full grow flex-col items-center">
+      <Container>
+        <section className="mb-[140px] flex w-full flex-col pb-10 pt-20 ">
+          <Title level={TitleLevel.Display3}>Join our team</Title>
+          <div className="flex w-full flex-col font-display text-sm text-white lg:text-xl">
+            <JobsList />
+          </div>
+        </section>
+      </Container>
     </main>
   );
 }
