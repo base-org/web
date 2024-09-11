b98d9ac4b631b2b10419d645293000d8aa8944a9
Léo Galley <contact@kirkas.ch>
2024-09-11 14:09:44 -0400

    about page

--- apps/web/src/components/StartBuildingOnBase/StartBuildingOnBase.tsx
+++ apps/web/src/components/StartBuildingOnBase/StartBuildingOnBase.tsx
@@ -1,4 +1,9 @@
-import { Button, ButtonVariants } from 'apps/web/src/components/Button/Button';
+import Button from 'apps/web/src/components/base-org/Button';
+import { ButtonSizes, ButtonVariants } from 'apps/web/src/components/base-org/Button/types';
+import Container from 'apps/web/src/components/base-org/Container';
+import Title from 'apps/web/src/components/base-org/typography/Title';
+import { TitleLevel } from 'apps/web/src/components/base-org/typography/Title/types';
+
 import ImageAdaptive from 'apps/web/src/components/ImageAdaptive';
 import { docsUrl } from 'apps/web/src/constants';
 import Link from 'next/link';
@@ -7,7 +12,9 @@ async function ReadTheDocsButton() {
   return (
     <div className="w-[200px]">
       <Link href={docsUrl} target="_blank" rel="noreferrer noopener">
-        <Button variant={ButtonVariants.Primary}>Read the docs</Button>
+        <Button variant={ButtonVariants.Secondary} size={ButtonSizes.Large}>
+          Read the docs
+        </Button>
       </Link>
     </div>
   );
@@ -15,20 +22,24 @@ async function ReadTheDocsButton() {
 
 export async function StartBuildingOnBase() {
   return (
-    <section className="flex w-full max-w-[1440px] flex-row flex-wrap items-start justify-between bg-black px-8 py-8">
-      <div className="pb-16 font-display text-3xl text-white md:text-5xl lg:text-6xl">
-        <h2 className="mb-[12px]">Start building on Base</h2>
-        <ReadTheDocsButton />
-      </div>
-      <div className="relative h-[460px] w-full max-w-[678px]">
-        <ImageAdaptive
-          src="/images/start-building-on-base.png"
-          className="object-contain"
-          alt="Start building on base"
-          fill
-          quality={100}
-        />
-      </div>
-    </section>
+    <Container>
+      <section className="flex w-full flex-col items-start  justify-between bg-black py-8 md:flex-row">
+        <div className="py-16">
+          <Title level={TitleLevel.Display2} className="mb-12">
+            Start building on Base
+          </Title>
+          <ReadTheDocsButton />
+        </div>
+        <div className="relative h-[460px] w-full max-w-[678px]">
+          <ImageAdaptive
+            src="/images/start-building-on-base.png"
+            className="object-contain"
+            alt="Start building on base"
+            fill
+            quality={100}
+          />
+        </div>
+      </section>
+    </Container>
   );
 }
