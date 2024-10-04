ada6d8b7fab190f179314fa790c1025a048930b2
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2024-10-04 14:46:32 -0500

    check for frame ip safety (#1036)
    * check for ip safety
    
    * add jest
    
    * add gh workflow
    
    * middleware->frame calls with dns lookup

---
+++ apps/web/jest.config.js
@@ -0,0 +1,17 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
