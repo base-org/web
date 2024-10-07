5e2cd3c98d1b18087ce936fd2f82455cb266befa
Jordan Frankfurt <jordan.frankfurt@coinbase.com>
2024-10-07 10:48:50 -0500

    switch order, switch icon

--- apps/web/src/components/Basenames/UsernameProfileSectionFrames/FrameListItem.tsx
+++ apps/web/src/components/Basenames/UsernameProfileSectionFrames/FrameListItem.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { EllipsisHorizontalIcon, ClipboardIcon } from '@heroicons/react/24/outline';
+import { EllipsisHorizontalIcon } from '@heroicons/react/24/outline';
 import * as Popover from '@radix-ui/react-popover';
 import { useAnalytics } from 'apps/web/contexts/Analytics';
 import { useUsernameProfile } from 'apps/web/src/components/Basenames/UsernameProfileContext';
@@ -11,6 +11,7 @@ import Image, { StaticImageData } from 'next/image';
 import { useCallback } from 'react';
 import TrashIcon from './assets/trash-icon.svg';
 import { useCopyToClipboard } from 'usehooks-ts';
+import { Icon } from 'apps/web/src/components/Icon/Icon';
 
 export default function FrameListItem({ url }: { url: string }) {
   const { removeFrame } = useFrameContext();
@@ -48,6 +49,14 @@ export default function FrameListItem({ url }: { url: string }) {
             className="data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade rounded-xl bg-white p-2 will-change-[transform,opacity]"
             sideOffset={5}
           >
+            <button
+              type="button"
+              aria-label="copy frame url"
+              onClick={handleCopyFrameURLClick}
+              className="flex flex-row items-center justify-start gap-2 px-2 py-1"
+            >
+              <Icon name="copy" color="currentColor" width={16} /> Copy frame URL
+            </button>
             {currentWalletIsProfileOwner && (
               <button
                 type="button"
@@ -58,14 +67,6 @@ export default function FrameListItem({ url }: { url: string }) {
                 <Image alt="" src={TrashIcon as StaticImageData} width={16} /> Remove frame
               </button>
             )}
-            <button
-              type="button"
-              aria-label="copy frame url"
-              onClick={handleCopyFrameURLClick}
-              className="flex flex-row items-center justify-start gap-2 px-2 py-1"
-            >
-              <ClipboardIcon width={16} /> Copy frame URL
-            </button>
           </Popover.Content>
         </Popover.Portal>
       </Popover.Root>
