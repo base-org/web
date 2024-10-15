90aadeb401671e6768651fa7ddf3b337f456caa7
Brendan Forster <brendan.forster@coinbase.com>
2024-10-15 12:57:15 -0700

    basic UI for grant submission

--- apps/web/app/(base-org)/grants/page.tsx
+++ apps/web/app/(base-org)/grants/page.tsx
@@ -3,12 +3,13 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useAccount } from 'wagmi';
 import AnalyticsProvider from 'apps/web/contexts/Analytics';
-import Container from 'apps/web/src/components/base-org/Container';
+import Container from 'apps/web/src/components/base-org/Container/index';
 import useBaseEnsName from 'apps/web/src/hooks/useBaseEnsName';
 import useReadBaseEnsTextRecords from 'apps/web/src/hooks/useReadBaseEnsTextRecords';
 import { Button } from 'apps/web/src/components/Button/Button';
+import { appendGrantApplicationToGoogleSheet } from 'apps/web/src/utils/googleApi/appendToGoogleSheet';
 
-type GrantApplicationData = {
+export type GrantApplicationData = {
   builderName: string;
   builderTwitter: string;
   builderFarcaster: string;
@@ -43,6 +44,7 @@ const sheetId = '0';
 export default function GrantApplication() {
   const [grantApp, setGrantApp] = useState(blankGrantApplication);
   const [textRecordsLoading, setTextRecordsLoading] = useState(true);
+  const [formSuccess, setFormSuccess] = useState(false);
 
   const { address } = useAccount();
   const { data: username } = useBaseEnsName({ address });
@@ -57,12 +59,12 @@ export default function GrantApplication() {
   );
 
   useEffect(() => {
-    setGrantApp({
-      ...grantApp,
+    setGrantApp((prevGrantApp) => ({
+      ...prevGrantApp,
       builderName: username,
-      builderTwitter: textRecords.existingTextRecords['com.twitter'],
-      builderFarcaster: textRecords.existingTextRecords['xyz.farcaster'],
-    });
+      builderTwitter: memoizedTextRecords.twitter,
+      builderFarcaster: memoizedTextRecords.farcaster,
+    }));
   }, [username, memoizedTextRecords]);
 
   useEffect(() => {
@@ -72,8 +74,14 @@ export default function GrantApplication() {
   }, [textRecords.existingTextRecordsIsLoading]);
 
   const handleClick = useCallback(() => {
-    appendToGSheet(sheetId, grantApp)
-  }, [sheetId, grantApp]);
+    appendGrantApplicationToGoogleSheet(grantApp, reqUrl, sheetId)
+      .then((res) => {
+        if (res.success) {
+          setFormSuccess(true);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, [grantApp]);
 
   if (textRecordsLoading) {
     return;
@@ -83,106 +91,31 @@ export default function GrantApplication() {
     <AnalyticsProvider context="grants">
       <div className="pb-16 pt-48">
         <Container>
-          <h1 className="text-4xl">Apply for a Base Builder Grant</h1>
-          <ul>
-            <li>Builder Name: {grantApp.builderName}</li>
-            <li>Builder Twitter: {grantApp.builderTwitter}</li>
-            <li>Builder Farcaster: {grantApp.builderFarcaster}</li>
-            <li>Builder Email: {grantApp.builderEmail}</li>
-            <li>Project Name: {grantApp.projectName}</li>
-            <li>Project URL: {grantApp.projectUrl}</li>
-            <li>Project Twitter: {grantApp.projectTwitter}</li>
-            <li>Project Farcaster: {grantApp.projectFarcaster}</li>
-            <li>Project Demo Link: {grantApp.projectDemoLink}</li>
-            <li>Project Live on Base: {grantApp.liveOnBase ? 'true' : 'false'}</li>
-            <li>Reasoning: {grantApp.reasoning}</li>
-          </ul>
-          <Button onClick={handleClick} className="mt-12">
-            Submit
-          </Button>
+          {formSuccess ? (
+            <div>Yay!</div>
+          ) : (
+            <>
+              <h1 className="text-4xl">Apply for a Base Builder Grant</h1>
+              <ul>
+                <li>Builder Name: {grantApp.builderName}</li>
+                <li>Builder Twitter: {grantApp.builderTwitter}</li>
+                <li>Builder Farcaster: {grantApp.builderFarcaster}</li>
+                <li>Builder Email: {grantApp.builderEmail}</li>
+                <li>Project Name: {grantApp.projectName}</li>
+                <li>Project URL: {grantApp.projectUrl}</li>
+                <li>Project Twitter: {grantApp.projectTwitter}</li>
+                <li>Project Farcaster: {grantApp.projectFarcaster}</li>
+                <li>Project Demo Link: {grantApp.projectDemoLink}</li>
+                <li>Project Live on Base: {grantApp.liveOnBase ? 'true' : 'false'}</li>
+                <li>Reasoning: {grantApp.reasoning}</li>
+              </ul>
+              <Button onClick={handleClick} className="mt-12">
+                Submit
+              </Button>
+            </>
+          )}
         </Container>
       </div>
     </AnalyticsProvider>
   );
 }
-
-const appendToGSheet = async (sheetId: string, app: GrantApplicationData) => {
-  const reqBody = {
-    requests: [
-      {
-        appendCells: {
-          sheetId: sheetId,
-          rows: [
-            {
-              values: [
-                { userEnteredValue: { stringValue: app.builderName } },
-                {
-                  userEnteredValue: {
-                    stringValue: app.builderEmail,
-                  },
-                },
-                { userEnteredValue: { stringValue: app.builderTwitter } },
-                {
-                  userEnteredValue: {
-                    stringValue: app.builderFarcaster,
-                  },
-                },
-                {
-                  userEnteredValue: {
-                    stringValue: app.projectName,
-                  },
-                },
-                {
-                  userEnteredValue: {
-                    stringValue: app.projectUrl,
-                  },
-                },
-                {
-                  userEnteredValue: {
-                    stringValue: app.projectTwitter,
-                  },
-                },
-                {
-                  userEnteredValue: {
-                    stringValue: app.projectFarcaster,
-                  },
-                },
-                { userEnteredValue: { boolValue: app.liveOnBase } },
-                {
-                  userEnteredValue: {
-                    stringValue: app.reasoning,
-                  },
-                },
-                {
-                  userEnteredValue: {
-                    stringValue: app.projectDemoLink,
-                  },
-                },
-              ],
-            },
-          ],
-          fields: '*',
-        },
-      },
-    ],
-  };
-
-  const accessToken =
-    'ya29.a0AcM612zZXHAgnrClyhkaHTsMhDJj0pq7lgDwwnN42Osl2sSS3lXKoeK1m5pkZuY7TzhFDNB6iO2Kg4yiL9Vy8C1jP58_YkU4Ss_AnCVSatp6aSmJWZ85tWfEE8XBg-BDObxVPoNbM1L7AiGx3HFILJScW44aAa2jrEl14Dd0aCgYKAQoSARASFQHGX2MiKfOIS1XtKmc-n9UCvqVuSw0175';
-  const config = {
-    method: 'POST',
-    body: JSON.stringify(reqBody),
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      'Content-Type': 'application/json',
-    },
-  };
-
-  try {
-    const res = await fetch(reqUrl, config);
-    const data = await res.json();
-    console.log({data})
-  } catch (error) {
-    console.error('Error:', error);
-  }
-};
