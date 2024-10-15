b3656f3b586eccd0dc0e927bbeaece6853950252
Brendan Forster <brendan.forster@coinbase.com>
2024-10-14 17:09:22 -0700

    adjusted hoverstate

--- apps/web/src/components/base-org/root/VideoCardsSection/index.tsx
+++ apps/web/src/components/base-org/root/VideoCardsSection/index.tsx
@@ -23,7 +23,7 @@ export default function VideoCardsSection() {
         description={
           <span>
             Base is built as an Ethereum L2, decentralized with the{' '}
-            <Link href="https://optimism.io/build" className="hover:text-blue-30">
+            <Link href="https://optimism.io/build" className="hover:underline">
               Optimism Superchain
             </Link>
             , and incubated by Coinbase.
