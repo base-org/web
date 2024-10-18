e5ce70d83447e2d47830ef6ec14007a7a3d3383a
Brendan Forster <brendan.forster@coinbase.com>
2024-10-18 11:51:30 -0700

    better error handling in form

--- apps/web/src/components/Grants/GrantApplicationForm.tsx
+++ apps/web/src/components/Grants/GrantApplicationForm.tsx
@@ -1,4 +1,5 @@
-import { Dispatch, SetStateAction, useCallback, useEffect, useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useEffect, useMemo, useState } from 'react';
+import classNames from 'classnames';
 import Button from 'apps/web/src/components/base-org/Button';
 import { appendGrantApplicationToGoogleSheet } from 'apps/web/src/utils/googleApi/appendToGoogleSheet';
 import {
@@ -14,6 +15,8 @@ type GrantApplictionFormProps = {
   };
   formSetter: Dispatch<SetStateAction<FormStates>>;
 };
+
+type InputChangeHandler = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
 
 const blankGrantApplication: GrantApplicationData = {
   nomineeName: '',
@@ -38,6 +41,7 @@ export default function GrantApplicationForm({
   formSetter,
 }: GrantApplictionFormProps) {
   const [grantApp, setGrantApp] = useState(blankGrantApplication);
+  const isFormValid = useMemo(() => validateGrantApplication(grantApp), [grantApp]);
 
   useEffect(() => {
     setGrantApp((prevGrantApp) => ({
@@ -53,7 +57,8 @@ export default function GrantApplicationForm({
       const target = e.target;
       setGrantApp((prevGrantApp) => ({
         ...prevGrantApp,
-        [target.name]: target.type === "checkbox" ? (target as HTMLInputElement).checked : target.value,
+        [target.name]:
+          target.type === 'checkbox' ? (target as HTMLInputElement).checked : target.value,
       }));
     },
     [],
@@ -74,85 +79,58 @@ export default function GrantApplicationForm({
       .catch((err) => console.error(err));
   }, [grantApp, formSetter]);
 
-  console.log({ grantApp });
-
   return (
     <div>
       <div className="my-6">
-        <div className="my-4">
-          <span className="pr-6">Nominee Basename:</span>
-          <input
-            type="text"
-            name="nomineeName"
-            value={grantApp.nomineeName}
-            onChange={handleChange}
-            className="w-full p-2 text-black"
-          />
-        </div>
-        <div className="my-4">
-          <span className="pr-6">Nominee Twitter:</span>
-          <input
-            type="text"
-            name="nomineeTwitter"
-            value={grantApp.nomineeTwitter}
-            onChange={handleChange}
-            className="w-full p-2 text-black"
-          />
-        </div>
-        <div className="my-4">
-          <span className="pr-6">Nominee Farcaster:</span>
-          <input
-            type="text"
-            name="nomineeFarcaster"
-            value={grantApp.nomineeFarcaster}
-            onChange={handleChange}
-            className="w-full p-2 text-black"
-          />
-        </div>
-        <div className="my-4">
-          <span className="pr-6">Nominee Email:</span>
-          <input
-            type="text"
-            name="nomineeEmail"
-            value={grantApp.nomineeEmail}
-            onChange={handleChange}
-            className="w-full p-2 text-black"
-            placeholder="based@builder.xyz"
-          />
-        </div>
-        <div className="my-4">
-          <span className="pr-6">Project Name:</span>
-          <input
-            type="text"
-            name="projectName"
-            value={grantApp.projectName}
-            onChange={handleChange}
-            className="w-full p-2 text-black"
-            placeholder="Based Project"
-          />
-        </div>
-        <div className="my-4">
-          <span className="pr-6">Project URL:</span>
-          <input
-            type="text"
-            name="projectUrl"
-            value={grantApp.projectUrl}
-            onChange={handleChange}
-            className="w-full p-2 text-black"
-            placeholder="basedproject.xyz"
-          />
-        </div>
-        <div className="my-4">
-          <span className="pr-6">Please link a 1 minute demo of the project:</span>
-          <input
-            type="text"
-            name="projectDemoLink"
-            value={grantApp.projectDemoLink}
-            onChange={handleChange}
-            className="w-full p-2 text-black"
-            placeholder="demo.basedproject.xyz"
-          />
-        </div>
+        <FormField
+          fieldPrompt="Nominee Basename"
+          fieldName="nomineeName"
+          fieldPlaceholder="basedbuilder.base.eth"
+          application={grantApp}
+          changeHandler={handleChange}
+        />
+        <FormField
+          fieldPrompt="Nominee Twitter"
+          fieldName="nomineeTwitter"
+          fieldPlaceholder="@based_builder_twitter"
+          application={grantApp}
+          changeHandler={handleChange}
+        />
+        <FormField
+          fieldPrompt="Nominee Farcaster"
+          fieldName="nomineeFarcaster"
+          fieldPlaceholder="@based_builder_farcaster"
+          application={grantApp}
+          changeHandler={handleChange}
+        />
+        <FormField
+          fieldPrompt="Nominee Email"
+          fieldName="nomineeEmail"
+          fieldPlaceholder="based.builder@basedproject.xyz"
+          application={grantApp}
+          changeHandler={handleChange}
+        />
+        <FormField
+          fieldPrompt="Project Name"
+          fieldName="projectName"
+          fieldPlaceholder="Based Project"
+          application={grantApp}
+          changeHandler={handleChange}
+        />
+        <FormField
+          fieldPrompt="Project URL"
+          fieldName="projectUrl"
+          fieldPlaceholder="basedproject.xyz"
+          application={grantApp}
+          changeHandler={handleChange}
+        />
+        <FormField
+          fieldPrompt="Please link a 1 minute demo of the project:"
+          fieldName="projectDemoLink"
+          fieldPlaceholder="basedproject.xyz/demo"
+          application={grantApp}
+          changeHandler={handleChange}
+        />
         <div className="my-4">
           <span className="pr-6">Why does this project deserve a base Grant?</span>
           <textarea
@@ -163,19 +141,15 @@ export default function GrantApplicationForm({
             placeholder="Limit response to 150 words"
           />
         </div>
-        <div className="my-4 flex flex-row justify-start">
-          <span className="pr-6">Is this project live on Base?</span>
-          <input
-            type="checkbox"
-            name="liveOnBase"
-            checked={grantApp.liveOnBase}
-            onChange={handleChange}
-            className="w-6 text-black"
-          />
-        </div>
-        <div className="my-4 flex flex-col justify-start gap-2">
-          <span className="pr-6">
-            By clicking this box, you hereby authorize and license to Coinbase on a non-exclusive,
+        <FormField
+          fieldPrompt="By clicking this box, you confirm that this project is currently live on Base."
+          fieldName="liveOnBase"
+          fieldType="checkbox"
+          application={grantApp}
+          changeHandler={handleChange}
+        />
+        <FormField
+          fieldPrompt="By clicking this box, you hereby authorize and license to Coinbase on a non-exclusive,
             worldwide, irrevocable, sublicensable, and royalty free basis during to reproduce,
             distribute, transmit, make available, perform, display, or otherwise use the submitted
             Multimedia Assets for any purpose, including any marketing or promotional activities
@@ -183,42 +157,117 @@ export default function GrantApplicationForm({
             your Multimedia Assets will inure to your benefit. You further acknowledge and represent
             that you have all IP rights in the Multimedia Assets, that the Multimedia Assets do not
             infringe the rights of any third party, and that you have the right to grant this
-            license to Coinbase.
-          </span>
-          <div className="flex flex-row">
-            <span className="pr-6">I confirm</span>
-            <input
-              type="checkbox"
-              name="acceptedLegalDisclaimer"
-              checked={grantApp.acceptedLegalDisclaimer}
-              onChange={handleChange}
-              className="w-6 text-black"
-            />
-          </div>
-        </div>
+            license to Coinbase."
+          fieldName="acceptedLegalDisclaimer"
+          fieldType="checkbox"
+          application={grantApp}
+          changeHandler={handleChange}
+        />
       </div>
-      <Button onClick={handleClick} className="mt-12">
+      <Button onClick={handleClick} className="mt-12" disabled={!isFormValid}>
         Submit
       </Button>
     </div>
   );
 }
 
+function FormField({
+  fieldType = 'text',
+  fieldPrompt,
+  fieldName,
+  fieldPlaceholder,
+  application,
+  changeHandler,
+}: {
+  fieldType?: 'text' | 'checkbox';
+  fieldPrompt: string;
+  fieldName: keyof GrantApplicationData;
+  fieldPlaceholder?: string;
+  application: GrantApplicationData;
+  changeHandler: InputChangeHandler;
+}) {
+  const [error, setError] = useState(false);
+
+  const handleBlur = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      const isValid = validateFormField(e.target.name, application);
+      if (!isValid) {
+        setError(true);
+      } else {
+        setError(false);
+      }
+    },
+    [application],
+  );
+
+  if (fieldType === 'text') {
+    return (
+      <div className="my-4">
+        <div>
+          <span className="pr-6">{fieldPrompt}:</span>
+          <span className={classNames('text-red-50', { hidden: !error })}>
+            * This field is required.
+          </span>
+        </div>
+        <input
+          type={fieldType}
+          name={fieldName}
+          value={application[fieldName] as string}
+          placeholder={fieldPlaceholder}
+          onChange={changeHandler}
+          onBlur={handleBlur}
+          className={classNames('w-full p-2 text-black', {
+            'border-2 border-red-60': error,
+          })}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className="my-4 flex flex-col justify-start gap-2">
+      <span className="pr-6">{fieldPrompt}</span>
+      <div className="flex flex-row">
+        <span className="pr-6">I confirm</span>
+        <input
+          type={fieldType}
+          name={fieldName}
+          checked={application[fieldName] as boolean}
+          onChange={changeHandler}
+          onBlur={handleBlur}
+          className="w-6 text-black"
+        />
+        <span className={classNames('text-red-50', { hidden: !error })}>
+          * This field is required.
+        </span>
+      </div>
+    </div>
+  );
+}
+
 function validateGrantApplication(grantApp: GrantApplicationData) {
   for (const key in grantApp) {
-    if (!isValidGrantApplicationKey(key)) {
-      return false;
-    }
-    if (key === 'nomineeEmail') {
-      if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(grantApp[key])) {
-        return false;
-      }
-    }
-    if (!grantApp[key]) {
+    const isValid = validateFormField(key, grantApp);
+    if (!isValid) {
       return false;
     }
   }
 
+  return true;
+}
+
+function validateFormField(key: string, grantApp: GrantApplicationData) {
+  if (!isValidGrantApplicationKey(key)) {
+    return false;
+  }
+  if (key === 'nomineeEmail') {
+    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(grantApp[key])) {
+      return false;
+    }
+  }
+  if (!grantApp[key]) {
+    return false;
+  }
   return true;
 }
 
