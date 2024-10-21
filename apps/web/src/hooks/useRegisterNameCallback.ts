7dcd856c04864836093a40132e2006e718da5ce0
Léo Galley <contact@kirkas.ch>
2024-10-21 09:50:07 -0400

    Feat: Registration Discount codes (#1084)
    * discount code temp
    
    * comments
    
    * BE: Proof of concept
    
    * mark/increment code as consumed
    
    * bad code
    
    * bad code
    
    * dry it up
    
    * remove test code
    
    * cleanup and fix
    
    * suspense
    
    * undo change
    
    * refactor: use internal transaction/capacities hooks for registration flow
    
    * refetch basename
    
    * fix: wrong name being used when reverseRecord is false
    
    * prefetch
    
    * ssr code instead of search params
    
    * add banner & code

--- apps/web/src/hooks/useRegisterNameCallback.ts
+++ apps/web/src/hooks/useRegisterNameCallback.ts
@@ -1,9 +1,11 @@
-import { useAnalytics } from 'apps/web/contexts/Analytics';
 import { useErrors } from 'apps/web/contexts/Errors';
 import L2ResolverAbi from 'apps/web/src/abis/L2Resolver';
 import { USERNAME_L2_RESOLVER_ADDRESSES } from 'apps/web/src/addresses/usernames';
 import useBaseEnsName from 'apps/web/src/hooks/useBaseEnsName';
 import useBasenameChain from 'apps/web/src/hooks/useBasenameChain';
+import useCapabilitiesSafe from 'apps/web/src/hooks/useCapabilitiesSafe';
+import useWriteContractsWithLogs from 'apps/web/src/hooks/useWriteContractsWithLogs';
+import useWriteContractWithReceipt from 'apps/web/src/hooks/useWriteContractWithReceipt';
 import {
   formatBaseEthDomain,
   IS_EARLY_ACCESS,
@@ -11,26 +13,14 @@ import {
   REGISTER_CONTRACT_ABI,
   REGISTER_CONTRACT_ADDRESSES,
 } from 'apps/web/src/utils/usernames';
-import { ActionType } from 'libs/base-ui/utils/logEvent';
-import { Dispatch, SetStateAction, useCallback, useMemo, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { encodeFunctionData, namehash } from 'viem';
-import { useAccount, useSwitchChain, useWriteContract } from 'wagmi';
-import { useCapabilities, useWriteContracts } from 'wagmi/experimental';
+import { useAccount } from 'wagmi';
 
 function secondsInYears(years: number): bigint {
   const secondsPerYear = 365.25 * 24 * 60 * 60; // .25 accounting for leap years
   return BigInt(Math.round(years * secondsPerYear));
 }
-
-type UseRegisterNameCallbackReturnValue = {
-  callback: () => Promise<void>;
-  data: `0x${string}` | undefined;
-  isPending: boolean;
-  error: string | undefined | null;
-  reverseRecord: boolean;
-  setReverseRecord: Dispatch<SetStateAction<boolean>>;
-  hasExistingBasename: boolean;
-};
 
 export function useRegisterNameCallback(
   name: string,
@@ -38,16 +28,15 @@ export function useRegisterNameCallback(
   years: number,
   discountKey?: `0x${string}`,
   validationData?: `0x${string}`,
-): UseRegisterNameCallbackReturnValue {
-  const { address, chainId, isConnected, connector } = useAccount();
+) {
+  const { address } = useAccount();
   const { basenameChain } = useBasenameChain();
   const { logError } = useErrors();
-  const {
-    writeContractsAsync,
-    isPending: paymasterIsPending,
-    error: paymasterError,
-  } = useWriteContracts();
+  const { paymasterServiceEnabled } = useCapabilitiesSafe({
+    chainId: basenameChain.id,
+  });
 
+  // If user has a basename, reverse record is set to false
   const { data: baseEnsName, isLoading: baseEnsNameIsLoading } = useBaseEnsName({
     address,
   });
@@ -59,41 +48,31 @@ export function useRegisterNameCallback(
 
   const [reverseRecord, setReverseRecord] = useState<boolean>(!hasExistingBasename);
 
-  const isCoinbaseSmartWallet = connector?.id === 'coinbase';
-  const paymasterEnabled = isCoinbaseSmartWallet;
+  // Transaction with paymaster enabled
+  const { initiateBatchCalls, batchCallsStatus, batchCallsIsLoading, batchCallsError } =
+    useWriteContractsWithLogs({
+      chain: basenameChain,
+      eventName: 'register_name',
+    });
 
-  const { data, writeContractAsync, isPending, error } = useWriteContract();
-  const { data: availableCapacities } = useCapabilities({
-    account: address,
-    query: { enabled: isConnected && paymasterEnabled },
+  // Transaction without paymaster
+  const {
+    initiateTransaction: initiateRegisterName,
+    transactionStatus: registerNameStatus,
+    transactionIsLoading: registerNameIsLoading,
+    transactionError: registerNameError,
+  } = useWriteContractWithReceipt({
+    chain: basenameChain,
+    eventName: 'register_name',
   });
 
-  const capabilities = useMemo(() => {
-    if (!isConnected || !chainId || !availableCapacities) {
-      return {};
-    }
-    const chainCapabilities = availableCapacities[chainId];
-    if (chainCapabilities.paymasterService?.supported) {
-      return {
-        paymasterService: {
-          // url: `${document.location.origin}/api/paymaster`
-        },
-      };
-    }
-    return {};
-  }, [availableCapacities, chainId, isConnected]);
-
+  // Params
   const normalizedName = normalizeEnsDomainName(name);
-  const { switchChainAsync } = useSwitchChain();
   const isDiscounted = Boolean(discountKey && validationData);
-  const { logEventWithContext } = useAnalytics();
 
+  // Callback
   const registerName = useCallback(async () => {
     if (!address) return;
-    if (chainId !== basenameChain.id) {
-      await switchChainAsync({ chainId: basenameChain.id });
-      return;
-    }
 
     const addressData = encodeFunctionData({
       abi: L2ResolverAbi,
@@ -119,22 +98,17 @@ export function useRegisterNameCallback(
       reverseRecord, // Bool to decide whether to set this name as the "primary" name for the `owner`.
     };
 
-    // Log attempt to register name
-    logEventWithContext('register_name_transaction_initiated', ActionType.click);
-
     try {
-      if (!capabilities || Object.keys(capabilities).length === 0) {
-        await writeContractAsync({
+      if (!paymasterServiceEnabled) {
+        await initiateRegisterName({
           abi: REGISTER_CONTRACT_ABI,
           address: REGISTER_CONTRACT_ADDRESSES[basenameChain.id],
-          chainId: basenameChain.id,
           functionName: isDiscounted || IS_EARLY_ACCESS ? 'discountedRegister' : 'register',
-          // @ts-expect-error isDiscounted is sufficient guard for discountKey and validationData presence
           args: isDiscounted ? [registerRequest, discountKey, validationData] : [registerRequest],
           value,
         });
       } else {
-        await writeContractsAsync({
+        await initiateBatchCalls({
           contracts: [
             {
               abi: REGISTER_CONTRACT_ABI,
@@ -143,46 +117,41 @@ export function useRegisterNameCallback(
               args: isDiscounted
                 ? [registerRequest, discountKey, validationData]
                 : [registerRequest],
-              // @ts-expect-error writeContractsAsync is incorrectly typed to not accept value
               value,
             },
           ],
-          capabilities: capabilities,
-          chainId: basenameChain.id,
+          account: address,
+          chain: basenameChain,
         });
       }
     } catch (e) {
       logError(e, 'Register name transaction canceled');
-      logEventWithContext('register_name_transaction_canceled', ActionType.change);
     }
   }, [
     address,
-    chainId,
-    basenameChain.id,
+    basenameChain,
+    discountKey,
+    initiateBatchCalls,
+    initiateRegisterName,
+    isDiscounted,
+    logError,
     name,
     normalizedName,
-    years,
+    paymasterServiceEnabled,
     reverseRecord,
-    logEventWithContext,
-    switchChainAsync,
-    capabilities,
-    writeContractAsync,
-    isDiscounted,
-    discountKey,
     validationData,
     value,
-    writeContractsAsync,
-    logError,
+    years,
   ]);
 
   return {
     callback: registerName,
-    data,
-    isPending: isPending ?? paymasterIsPending,
-    // @ts-expect-error error will be string renderable
-    error: error ?? paymasterError,
+    isPending: registerNameIsLoading || batchCallsIsLoading,
+    error: registerNameError ?? batchCallsError,
     reverseRecord,
     setReverseRecord,
     hasExistingBasename,
+    batchCallsStatus,
+    registerNameStatus,
   };
 }
