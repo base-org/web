b47d05f95ae7ec2d5d2d67aa8285b170f544e7d2
Léo Galley <contact@kirkas.ch>
2024-10-24 16:54:16 -0400

    replace copy (#1151)

--- apps/web/src/components/Basenames/UsernameProfileSectionBadges/Badges/index.tsx
+++ apps/web/src/components/Basenames/UsernameProfileSectionBadges/Badges/index.tsx
@@ -49,8 +49,7 @@ export const BADGE_INFO: Record<
   VERIFIED_IDENTITY: {
     name: 'Coinbase Verified ID',
     title: 'Coinbase Verified ID',
-    description:
-      "You've got a Coinbase account and you verified your ID. Thanks for being our customer.",
+    description: "You've got a Coinbase account and you verified your ID. Thanks for being legit!",
     cta: 'Get verified',
     ctaLink: 'https://coinbase.com/onchain-verify',
     image: verifiedIdentity,
