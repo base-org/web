58223e8a3103011da657723b4a8b6623a8fcb683
futreall <86553580+futreall@users.noreply.github.com>
2024-10-29 11:23:43 +0200

    Update Analytics.tsx
    Removed the reference to previousContext in AnalyticsProvider.
    Optimized context validation in useAnalytics for improved readability.
    Minor logging enhancements in logEventWithContext to provide more detailed data in development mode.
    AnalyticsProvider now correctly creates and provides fullContext without calling useAnalytics within itself

--- libs/base-ui/contexts/Analytics.tsx
+++ libs/base-ui/contexts/Analytics.tsx
@@ -10,36 +10,33 @@ export type AnalyticsContextProps = {
 };
 
 export const AnalyticsContext = createContext<AnalyticsContextProps>({
-  logEventWithContext: function () {
-    return undefined;
-  },
+  logEventWithContext: () => undefined,
   fullContext: '',
 });
 
 export function useAnalytics() {
   const context = useContext(AnalyticsContext);
-  if (context === undefined) {
-    throw new Error('useAnalytics must be used within a AnalyticsProvider');
+  if (!context) {
+    throw new Error('useAnalytics must be used within an AnalyticsProvider');
   }
   return context;
 }
 
 type AnalyticsProviderProps = {
   children?: ReactNode;
-  context: string; // TODO: This could be an enum in CCAEventData
+  context: string;
 };
 
 export default function AnalyticsProvider({ children, context }: AnalyticsProviderProps) {
-  const { fullContext: previousContext } = useAnalytics();
-
-  const fullContext = [previousContext, context].filter((c) => !!c).join('_');
+  const fullContext = context ? context : '';
+  
   const logEventWithContext = useCallback(
     (eventName: string, action: ActionType, eventData?: CCAEventData) => {
-      const sanitizedEventName = eventName.toLocaleLowerCase();
+      const sanitizedEventName = eventName.toLowerCase();
       if (typeof window === 'undefined') return;
 
       if (isDevelopment) {
-        return console.log('\nlogEventWithContext: \n', {
+        console.log('\nlogEventWithContext:', {
           eventName,
           sanitizedEventName,
           action,
@@ -47,25 +44,26 @@ export default function AnalyticsProvider({ children, context }: AnalyticsProvid
           page_path: window.location.pathname,
           ...eventData,
         });
+      } else {
+        logEvent(
+          sanitizedEventName,
+          {
+            action,
+            context: fullContext,
+            page_path: window.location.pathname,
+            ...eventData,
+          },
+          AnalyticsEventImportance.high,
+        );
       }
-
-      logEvent(
-        sanitizedEventName, // TODO: Do we want context here?
-        {
-          action: action,
-          context: fullContext,
-          page_path: window.location.pathname,
-          ...eventData,
-        },
-        AnalyticsEventImportance.high,
-      );
     },
     [fullContext],
   );
 
-  const values = useMemo(() => {
-    return { logEventWithContext, context, fullContext };
-  }, [context, fullContext, logEventWithContext]);
+  const values = useMemo(() => ({ logEventWithContext, fullContext }), [
+    fullContext,
+    logEventWithContext,
+  ]);
 
   return <AnalyticsContext.Provider value={values}>{children}</AnalyticsContext.Provider>;
 }
