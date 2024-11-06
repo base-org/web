af56c0bc3a5b1c4540bc6679c625c6cd87750a71
Matthew Bunday <matthew.bunday@coinbase.com>
2024-11-06 10:46:25 -0500

    Lint unused dep

--- apps/web/src/components/Basenames/ManageNames/NamesList.tsx
+++ apps/web/src/components/Basenames/ManageNames/NamesList.tsx
@@ -2,6 +2,8 @@
 
 import NameDisplay from './NameDisplay';
 import { useNameList } from 'apps/web/src/components/Basenames/ManageNames/hooks';
+import Link from 'apps/web/src/components/Link';
+import { Icon } from 'apps/web/src/components/Icon/Icon';
 
 export default function NamesList() {
   const { namesData, isLoading } = useNameList();
@@ -15,16 +17,27 @@ export default function NamesList() {
   }
 
   return (
-    <ul className="mx-auto max-w-2xl space-y-4 p-8">
-      {namesData.data.map((name) => (
-        <NameDisplay
-          key={name.token_id}
-          domain={name.domain}
-          isPrimary={name.is_primary}
-          tokenId={name.token_id}
-          expiresAt={name.expires_at}
-        />
-      ))}
-    </ul>
+    <div className="mx-auto max-w-2xl space-y-4 p-8">
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold">My Basenames</h1>
+        <Link
+          className="rounded-lg bg-palette-backgroundAlternate p-2 text-sm text-palette-foreground"
+          href="/names/"
+        >
+          <Icon name="plus" color="currentColor" width="12px" height="12px" />
+        </Link>
+      </div>
+      <ul className="mx-auto flex max-w-2xl flex-col gap-4">
+        {namesData.data.map((name) => (
+          <NameDisplay
+            key={name.token_id}
+            domain={name.domain}
+            isPrimary={name.is_primary}
+            tokenId={name.token_id}
+            expiresAt={name.expires_at}
+          />
+        ))}
+      </ul>
+    </div>
   );
 }
