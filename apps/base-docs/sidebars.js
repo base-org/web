95c3338b7977b7ca859437406de7ac1c88eab13d
kasrakhosravi <kasrakhosravi2@gmail.com>
2024-11-07 10:31:42 +0100

    fix: move erpc to node-providers section

--- apps/base-docs/sidebars.js
+++ apps/base-docs/sidebars.js
@@ -33,7 +33,6 @@ module.exports = {
       collapsed: false,
       items: [
         'tools/node-providers',
-        'tools/rpc-proxy-caching',
         'tools/block-explorers',
         'tools/network-faucets',
         'tools/oracles',
