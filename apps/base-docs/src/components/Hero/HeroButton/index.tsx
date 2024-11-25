571ba7f7d3b335f5c177267b4fc1f40f7359ccee
dschlabach <daniel.schlabach@coinbase.com>
2024-11-25 08:14:37 -0500

    format

--- apps/base-docs/src/components/Hero/HeroButton/index.tsx
+++ apps/base-docs/src/components/Hero/HeroButton/index.tsx
@@ -1,25 +1,25 @@
-import React, { useCallback } from "react";
+import React, { useCallback } from 'react';
 
-import logEvent from "base-ui/utils/logEvent";
+import logEvent from 'base-ui/utils/logEvent';
 
-import { heroEvent } from "../heroEvent";
-import styles from "./styles.module.css";
+import { heroEvent } from '../heroEvent';
+import styles from './styles.module.css';
 
 export default function HeroButton() {
-	const handleClick = useCallback(() => {
-		logEvent(heroEvent.name, heroEvent.event, heroEvent.importance);
-	}, [logEvent, heroEvent]);
+  const handleClick = useCallback(() => {
+    logEvent(heroEvent.name, heroEvent.event, heroEvent.importance);
+  }, [logEvent, heroEvent]);
 
-	return (
-		<a
-			href="https://base.org/build/?utm_source=basedocs&utm_medium=hero"
-			className={styles.cta}
-			target="_blank"
-			rel="noreferrer noopener"
-		>
-			<button className={styles.ctaButton} type="button" onClick={handleClick}>
-				Get Started
-			</button>
-		</a>
-	);
+  return (
+    <a
+      href="https://base.org/build/?utm_source=basedocs&utm_medium=hero"
+      className={styles.cta}
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      <button className={styles.ctaButton} type="button" onClick={handleClick}>
+        Get Started
+      </button>
+    </a>
+  );
 }
