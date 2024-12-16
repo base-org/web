783db20651fc917e8e1fc8c4aa81890d29f7e70a
francisojeah <ojeahfrancis@gmail.com>
2024-12-16 18:21:06 +0100

    chore: add Nigerian translations (Igbo, Yoruba, Hausa) of "Base is for everyone" on homepage

--- apps/web/src/components/base-org/root/SlidingTextSection/index.tsx
+++ apps/web/src/components/base-org/root/SlidingTextSection/index.tsx
@@ -8,7 +8,7 @@ import { CSSProperties, useRef } from 'react';
 export default function SlidingTextSection() {
   const containerRef = useRef<HTMLDivElement>(null);
 
-  const text = ' Base is for everyone - بایس للجمیع - Base es para todos - 基地适合所有人 - La Base è per tutti - Base est pour tout le monde - Base ni ya kila mtu - Base yɛ ma obiara - Base nye amesiame tɔ - Base ji he ni mɔ fɛɛ mɔ yɔɔ - Base är för alla - Base สำหรับทุกคน - ';
+  const text = ' Base is for everyone - بایس للجمیع - Base es para todos - 基地适合所有人 - La Base è per tutti - Base est pour tout le monde - Base ni ya kila mtu - Base yɛ ma obiara - Base nye amesiame tɔ - Base ji he ni mɔ fɛɛ mɔ yɔɔ - Base är för alla - Base สำหรับทุกคน - Base bụ maka onye ọ bụla - Base jẹ fun gbogbo eniyan - Base na kowa - ';
 
   const containerClasses = classNames(
     'relative w-full overflow-hidden rounded-2xl bg-blue p-8',
