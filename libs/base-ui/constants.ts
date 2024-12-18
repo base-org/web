9dc90c0b966b077708ff7853ad9bef3027d73512
Doner King <161360836+doner66@users.noreply.github.com>
2024-12-18 18:29:01 +0100

    Update constants.ts

--- libs/base-ui/constants.ts
+++ libs/base-ui/constants.ts
@@ -2,7 +2,7 @@ export const nodeEnv = process.env.NODE_ENV;
 export const docsUrl = process.env.DOCS_URL ?? 'https://docs.base.org';
 export const bridgeUrl = process.env.BRIDGE_URL ?? 'https://bridge.base.org';
 export const greenhouseApiUrl =
-  process.env.GREENHOUSE_HTTPS ?? 'https://boards-api.greenhouse.io/v1';
+  process.env.GREENHOUSE_HTTPS ?? 'https://boards-api.greenhouse.io';
 export const mainnetLaunchBlogPostURL =
   process.env.MAINNET_LAUNCH_BLOG_POST_URL ?? 'https://base.mirror.xyz/';
 export const mainnetLaunchFlag = process.env.MAINNET_LAUNCH_FLAG ?? 'false';
