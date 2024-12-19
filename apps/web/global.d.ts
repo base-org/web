e5b70db1dd34345a5812816554f78c4bd545666e
dschlabach <daniel.schlabach3@gmail.com>
2024-12-18 19:13:27 -0500

    fix

--- apps/web/global.d.ts
+++ apps/web/global.d.ts
@@ -1,40 +1,45 @@
-declare module 'next/config' {
-  type ConfigTypes = {
-    serverRuntimeConfig: {
-      alchemyApiKey: string;
-      alchemyApiUrl: string;
-    };
-    publicRuntimeConfig: {
-      nodeEnv: string;
-      docsUrl: string;
-      bridgeUrl: string;
-      greenhouseApiUrl: string;
-      mainnetLaunchBlogPostURL: string;
-      mainnetLaunchFlag: string;
-      ecosystemLaunchFlag: string;
-    };
-    i18n: {
-      locales: string[];
-      defaultLocale: string;
-    };
-  };
+declare module "next/config" {
+	type ConfigTypes = {
+		serverRuntimeConfig: {
+			alchemyApiKey: string;
+			alchemyApiUrl: string;
+		};
+		publicRuntimeConfig: {
+			nodeEnv: string;
+			docsUrl: string;
+			bridgeUrl: string;
+			greenhouseApiUrl: string;
+			mainnetLaunchBlogPostURL: string;
+			mainnetLaunchFlag: string;
+			ecosystemLaunchFlag: string;
+		};
+		i18n: {
+			locales: string[];
+			defaultLocale: string;
+		};
+	};
 
-  declare function getConfig(): ConfigTypes;
+	declare function getConfig(): ConfigTypes;
 
-  export default getConfig;
+	export default getConfig;
 }
 
-declare module '*.webm' {
-  const src: string;
-  export default src;
+declare module "*.webm" {
+	const src: string;
+	export default src;
 }
 
-declare module '*.glb' {
-  const src: string;
-  export default src;
+declare module "*.glb" {
+	const src: string;
+	export default src;
 }
 
-declare module '*.gltf' {
-  const src: string;
-  export default src;
+declare module "*.gltf" {
+	const src: string;
+	export default src;
+}
+
+declare module "*.mp4" {
+	const src: string;
+	export default src;
 }
