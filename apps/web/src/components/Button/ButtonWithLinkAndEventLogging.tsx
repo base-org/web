cd6710ca450af2a75f088f4c6f2f939d735bff90
footballlstore <139552589+footballlstore@users.noreply.github.com>
2025-01-26 16:49:25 +0200

    Update ButtonWithLinkAndEventLogging.tsx

--- apps/web/src/components/Button/ButtonWithLinkAndEventLogging.tsx
+++ apps/web/src/components/Button/ButtonWithLinkAndEventLogging.tsx
@@ -5,19 +5,21 @@ import { ActionType, ComponentType } from 'base-ui/utils/logEvent';
 import { useAnalytics } from '../../../contexts/Analytics';
 import { ButtonWithLink, ButtonWithLinkProps } from './ButtonWithLink';
 
-type ButtonWithLinkAndEventLogProps = Omit<ButtonWithLinkProps, 'onClick'> & {
+type ButtonWithLinkAndEventLogProps = ButtonWithLinkProps & {
   eventName: string;
 };
 
 export function ButtonWithLinkAndEventLogging({
   eventName,
+  onClick,
   ...buttonWithLinkProps
 }: ButtonWithLinkAndEventLogProps) {
   const { logEventWithContext } = useAnalytics();
 
   const handleClick = useCallback(() => {
     logEventWithContext(eventName, ActionType.click, { componentType: ComponentType.button });
-  }, [logEventWithContext, eventName]);
+    onClick?.();
+  }, [logEventWithContext, eventName, onClick]);
 
   return <ButtonWithLink onClick={handleClick} {...buttonWithLinkProps} />;
 }
