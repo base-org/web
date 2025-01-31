7318ce35932f4840203403c632855e1141c95998
Rudko Hanna <162179706+Pistasha@users.noreply.github.com>
2025-01-31 17:51:08 +0200

    Update route.ts

--- apps/web/app/(basenames)/api/basenames/avatar/ipfsUpload/route.ts
+++ apps/web/app/(basenames)/api/basenames/avatar/ipfsUpload/route.ts
@@ -28,7 +28,7 @@ export async function POST(request: NextRequest) {
     // referer can only be us
     // TODO: Won't work on vercel previews
     const refererUrl = new URL(referer);
-    const allowedReferersHost = isDevelopment ? 'localhost:3000' : 'www.base.org';
+    const allowedReferrersHost = isDevelopment ? 'localhost:3000' : 'www.base.org';
     if (allowedReferersHost !== refererUrl.host) {
       return NextResponse.json({ error: 'Invalid request' }, { status: 500 });
     }
