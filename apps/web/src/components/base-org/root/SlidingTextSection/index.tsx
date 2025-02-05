d8f5b7ec34299164e328e77f96c0c58264d1e03e
wbnns <will.binns@coinbase.com>
2025-02-04 23:31:34 -0100

    chore(homepage): Add Portugese translation

--- apps/web/src/components/base-org/root/SlidingTextSection/index.tsx
+++ apps/web/src/components/base-org/root/SlidingTextSection/index.tsx
@@ -9,7 +9,7 @@ export default function SlidingTextSection() {
   const containerRef = useRef<HTMLDivElement>(null);
 
   const text =
-    ' Base is for everyone - بایس للجمیع - Base es para todos - 基地适合所有人 - La Base è per tutti - Base est pour tout le monde - Base ni ya kila mtu - Base yɛ ma obiara - Base nye amesiame tɔ - Base ji he ni mɔ fɛɛ mɔ yɔɔ - Base är för alla - Base สำหรับทุกคน - Бейз для каждого - Бейз для кожного - Base ist für alle - Base herkes içindir - బేస్ అందరి కోసం - बेस सबके लिए है - ';
+    ' Base is for everyone - بایس للجمیع - Base es para todos - 基地适合所有人 - La Base è per tutti - Base est pour tout le monde - Base ni ya kila mtu - Base yɛ ma obiara - Base nye amesiame tɔ - Base ji he ni mɔ fɛɛ mɔ yɔɔ - Base är för alla - Base สำหรับทุกคน - Бейз для каждого - Бейз для кожного - Base ist für alle - Base herkes içindir - బేస్ అందరి కోసం - बेस सबके लिए है - Base é para todos - ';
 
   const containerClasses = classNames(
     'relative w-full overflow-hidden rounded-2xl bg-blue p-8',
