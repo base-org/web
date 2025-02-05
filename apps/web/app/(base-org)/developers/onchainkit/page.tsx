e585aeb6e0a84960c03ab75975b5e5947358315e
Alissa Crane <alissa.crane@coinbase.com>
2025-02-05 12:11:00 -0800

    add header icon

--- apps/web/app/(base-org)/developers/onchainkit/page.tsx
+++ apps/web/app/(base-org)/developers/onchainkit/page.tsx
@@ -8,13 +8,19 @@ import { InfoCards } from 'apps/web/src/components/Developers/Onchainkit/InfoCar
 import { Templates } from 'apps/web/src/components/Developers/Onchainkit/Templates';
 import { Testmonials } from 'apps/web/src/components/Developers/Onchainkit/Testimonials';
 import { CtaBanner } from 'apps/web/src/components/Developers/Shared/CtaBanner';
+import Image from 'next/image';
+import onchainkit from 'apps/web/src/components/Developers/Onchainkit/onchainkit.svg';
 
 export default async function OnchainKit() {
   return (
     <Container>
       <main className="mb-32 flex min-h-screen w-full flex-col items-center gap-40 bg-black pt-20">
         {/* Header  */}
-        <div className="flex flex-col items-center pt-20">
+        <div className="flex flex-col items-center gap-1 pt-20">
+          <div className="flex items-center gap-2 pb-6 text-[#C9A4FA]">
+            <Image src={onchainkit} alt="onchainkit" width={32} height={32} className="h-5 w-5" />
+            <Title level={TitleLevel.Title3}>OnchainKit</Title>
+          </div>
           <Title level={TitleLevel.Display3}>Full-stack onchain components</Title>
           <Title level={TitleLevel.Title3} className="text-gray-muted">
             All-you-need to build an onchain app in 10 minutes.
