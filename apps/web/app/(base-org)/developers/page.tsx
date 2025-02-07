c1e5de06dc929e0d3dd31fa645b5278eaf52c3ed
Brendan Forster <brendan.forster@coinbase.com>
2025-02-06 22:01:35 -0800

    override default container mobile padding

--- apps/web/app/(base-org)/developers/page.tsx
+++ apps/web/app/(base-org)/developers/page.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
 export default function Developers() {
   return (
     <AnalyticsProvider context="developers">
-      <Container>
+      <Container className="!px-[1.5rem] lg:!px-[2rem]">
         <main className="mb-32 flex min-h-screen w-full flex-col items-center bg-black">
           <Hero />
           <UseCases />
