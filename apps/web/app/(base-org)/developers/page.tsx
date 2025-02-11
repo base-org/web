7bc3e644d104b39dd0c0f07605e07b1848120271
Brendan Forster <brendan.forster@coinbase.com>
2025-02-11 10:45:11 -0800

    move spacing

--- apps/web/app/(base-org)/developers/page.tsx
+++ apps/web/app/(base-org)/developers/page.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from 'next';
 import AnalyticsProvider from 'apps/web/contexts/Analytics';
 import Container from 'apps/web/src/components/base-org/Container';
-import { Hero } from 'apps/web/src/components/Developers/Hero';
+import { Hero } from 'apps/web/src/components/Developers/components/Hero';
 import { UseCases } from 'apps/web/src/components/Developers/UseCases';
-import { Customers } from 'apps/web/src/components/Developers/Customers';
+import { Customers } from 'apps/web/src/components/Developers/components/Customers';
 import { Testimonials } from 'apps/web/src/components/Developers/Testimonials';
 import { Tools } from 'apps/web/src/components/Developers/Tools';
 import { WhyBase } from 'apps/web/src/components/Developers/WhyBase';
 import { LiveDemo } from 'apps/web/src/components/Developers/LiveDemo';
-import { BottomCta } from 'apps/web/src/components/Developers/BottomCta';
+import { BottomCta } from 'apps/web/src/components/Developers/components/BottomCta';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://base.org'),
@@ -23,7 +23,7 @@ export default function Developers() {
   return (
     <AnalyticsProvider context="developers">
       <Container className="!px-[1.5rem] lg:!px-[2rem]">
-        <main className="mb-32 pt-20 flex min-h-screen w-full flex-col items-center bg-black">
+        <main className="mb-32 flex min-h-screen w-full flex-col items-center bg-black pt-20">
           <Hero />
           <UseCases />
           <Customers />
