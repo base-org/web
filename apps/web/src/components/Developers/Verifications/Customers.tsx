4d60b654bb271b2fbf3574697319e4db74ee73ac
Alissa Crane <alissa.crane@coinbase.com>
2025-02-12 11:26:41 -0800

    add customers

--- apps/web/src/components/Developers/Verifications/Customers.tsx
+++ apps/web/src/components/Developers/Verifications/Customers.tsx
@@ -1,6 +1,10 @@
 import Title from 'apps/web/src/components/base-org/typography/Title';
 import { TitleLevel } from 'apps/web/src/components/base-org/typography/Title/types';
 import { Marquee } from 'apps/web/src/components/Developers/Shared/Marquee';
+import icebreaker from 'apps/web/src/components/Developers/Verifications/icebreaker.svg';
+import deform from 'apps/web/src/components/Developers/Verifications/deform.svg';
+import gitcoin from 'apps/web/src/components/Developers/Verifications/gitcoin.svg';
+import Image, { StaticImageData } from 'next/image';
 
 export function Customers() {
   return (
@@ -10,11 +14,16 @@ export function Customers() {
       </Title>
       <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
         <Marquee className="[--duration:20s]" pauseOnHover>
-          <div className="p-4">GUILD</div>
-          <div className="p-4">GUILD</div>
-          <div className="p-4">GUILD</div>
-          <div className="p-4">GUILD</div>
-          <div className="p-4">GUILD</div>
+          <div className="p-4">
+            <Image src={icebreaker as StaticImageData} alt="icebreaker" className="h-10 w-auto" />
+          </div>
+
+          <div className="p-4">
+            <Image src={deform as StaticImageData} alt="deform" className="h-10" />
+          </div>
+          <div className="p-4">
+            <Image src={gitcoin as StaticImageData} alt="gitcoin" className="h-10" />
+          </div>
         </Marquee>
       </div>
     </div>
