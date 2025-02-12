719df3ca0d9664a68b105bcf51162667636be111
Alissa Crane <alissa.crane@coinbase.com>
2025-02-12 11:53:54 -0800

    add customers

--- apps/web/src/components/Developers/BaseWallet/Customers.tsx
+++ apps/web/src/components/Developers/BaseWallet/Customers.tsx
@@ -2,10 +2,16 @@ import Title from 'apps/web/src/components/base-org/typography/Title';
 import { TitleLevel } from 'apps/web/src/components/base-org/typography/Title/types';
 import { Marquee } from 'apps/web/src/components/Developers/Shared/Marquee';
 import cattown from 'apps/web/src/components/Developers/BaseWallet/svg/cattown.svg';
-// import guild from 'apps/web/src/components/Developers/BaseWallet/svg/guild.svg';
-// import kleros from 'apps/web/src/components/Developers/BaseWallet/svg/kleros.svg';
-// import uniswap from 'apps/web/src/components/Developers/BaseWallet/svg/uniswap.svg';
-// import worldcoin from 'apps/web/src/components/Developers/BaseWallet/svg/worldcoin.svg';
+import aerodrome from 'apps/web/src/components/Developers/BaseWallet/svg/aerodrome.svg';
+import cooprecrods from 'apps/web/src/components/Developers/BaseWallet/svg/cooprecrods.svg';
+import frenpet from 'apps/web/src/components/Developers/BaseWallet/svg/frenpet.svg';
+import guild from 'apps/web/src/components/Developers/BaseWallet/svg/guild.svg';
+import heyelsa from 'apps/web/src/components/Developers/BaseWallet/svg/heyelsa.svg';
+import kyberswap from 'apps/web/src/components/Developers/BaseWallet/svg/kyberswap.svg';
+import layer3 from 'apps/web/src/components/Developers/BaseWallet/svg/layer3.svg';
+import mochicam from 'apps/web/src/components/Developers/BaseWallet/svg/mochicam.svg';
+import moonwell from 'apps/web/src/components/Developers/BaseWallet/svg/moonwell.svg';
+import opensea from 'apps/web/src/components/Developers/BaseWallet/svg/opensea.svg';
 import Image, { StaticImageData } from 'next/image';
 
 export function Customers() {
@@ -20,12 +26,38 @@ export function Customers() {
       <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
         <Marquee className="[--duration:20s]" pauseOnHover>
           <div className="p-4">
-            <Image src={cattown as StaticImageData} alt="header-image" className="h-10 w-10" />
+            <Image src={cattown as StaticImageData} alt="cattown" className="h-10 w-10" />
           </div>
-          <div className="p-4">GUILD</div>
-          <div className="p-4">GUILD</div>
-          <div className="p-4">GUILD</div>
-          <div className="p-4">GUILD</div>
+          <div className="p-4">
+            <Image src={aerodrome as StaticImageData} alt="aerodrome" className="h-10 w-auto" />
+          </div>
+          <div className="p-4">
+            <Image src={cooprecrods as StaticImageData} alt="cooprecrods" className="h-10 w-auto" />
+          </div>
+          <div className="p-4">
+            <Image src={frenpet as StaticImageData} alt="frenpet" className="h-10 w-auto" />
+          </div>
+          <div className="p-4">
+            <Image src={guild as StaticImageData} alt="guild" className="h-10 w-auto" />
+          </div>
+          <div className="p-4">
+            <Image src={heyelsa as StaticImageData} alt="heyelsa" className="h-10 w-auto" />
+          </div>
+          <div className="p-4">
+            <Image src={kyberswap as StaticImageData} alt="kyberswap" className="h-10 w-auto" />
+          </div>
+          <div className="p-4">
+            <Image src={layer3 as StaticImageData} alt="layer3" className="h-10 w-auto" />
+          </div>
+          <div className="p-4">
+            <Image src={mochicam as StaticImageData} alt="mochicam" className="h-10 w-auto" />
+          </div>
+          <div className="p-4">
+            <Image src={moonwell as StaticImageData} alt="moonwell" className="h-10 w-auto" />
+          </div>
+          <div className="p-4">
+            <Image src={opensea as StaticImageData} alt="opensea" className="h-10 w-auto" />
+          </div>
         </Marquee>
       </div>
     </div>
