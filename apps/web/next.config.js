ca8dad7dc3916026baf54a908ba9846ec3e0f7d8
Brendan from DeFi <brendan.forster@coinbase.com>
2025-02-13 13:19:17 -0800

    fix: update lottie csp (#1766)
    * update lottie csp
    
    * add lottie version
    
    * update csp

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -113,7 +113,9 @@ const contentSecurityPolicy = {
     'https://*.datadoghq.com', //datadog
     'https://translate.googleapis.com', // Let user translate our website
     'https://sdk-api.neynar.com/', // Neynar API
-    'https://unpkg.com/@lottiefiles/dotlottie-web@0.31.1/dist/dotlottie-player.wasm', // lottie player
+    'https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web@0.31.1/dist/dotlottie-player.wasm', // lottie player for profile pages
+    'https://unpkg.com/@lottiefiles/dotlottie-web@0.31.1/dist/dotlottie-player.wasm', // lottie player for profile pages
+    'https://unpkg.com/@lottiefiles/dotlottie-web@0.33.0/dist/dotlottie-player.wasm', // lottie player for api
     `https://${process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL}`,
     'https://usdc-claim-git-master-coinbase-vercel.vercel.app',
     'https://eth.merkle.io', // new default viem rpc
