179742eefea2da4f6807b07c91024687167966d0
Alissa Crane <alissa.crane@coinbase.com>
2025-02-12 21:33:31 -0800

    add tweets

--- apps/web/src/components/Developers/Onchainkit/Testimonials.tsx
+++ apps/web/src/components/Developers/Onchainkit/Testimonials.tsx
@@ -1,42 +1,23 @@
 import { Marquee } from 'apps/web/src/components/Developers/Shared/Marquee';
 import { TweetCard } from 'apps/web/src/components/Developers/Shared/TweetCard';
+import { TWEETS } from 'apps/web/src/components/Developers/Onchainkit/tweets';
 
 export function Testmonials() {
+  const mid = Math.ceil(TWEETS.length / 2);
+  const firstHalf = TWEETS.slice(0, mid);
+  const secondHalf = TWEETS.slice(mid);
+
   return (
     <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
       <Marquee className="[--duration:20s]" pauseOnHover>
-        <TweetCard
-          name="Rob at Cat.town"
-          username="@cattown"
-          content="I just became a huge fan of @OnchainKit!  If you're diving into onchain app development, this is hands down the best library to kickstart your journey."
-        />
-        <TweetCard
-          name="Rob at Cat.town"
-          username="@cattown"
-          content="I just became a huge fan of @OnchainKit!  If you're diving into onchain app development, this is hands down the best library to kickstart your journey."
-        />
-        <TweetCard
-          name="Rob at Cat.town"
-          username="@cattown"
-          content="I just became a huge fan of @OnchainKit!  If you're diving into onchain app development, this is hands down the best library to kickstart your journey."
-        />
+        {firstHalf?.map((tweet) => (
+          <TweetCard key={tweet.username} {...tweet} />
+        ))}
       </Marquee>
       <Marquee className="[--duration:20s]" reverse pauseOnHover>
-        <TweetCard
-          name="Rob at Cat.town"
-          username="@cattown"
-          content="I just became a huge fan of @OnchainKit!  If you're diving into onchain app development, this is hands down the best library to kickstart your journey."
-        />
-        <TweetCard
-          name="Rob at Cat.town"
-          username="@cattown"
-          content="I just became a huge fan of @OnchainKit!  If you're diving into onchain app development, this is hands down the best library to kickstart your journey."
-        />
-        <TweetCard
-          name="Rob at Cat.town"
-          username="@cattown"
-          content="I just became a huge fan of @OnchainKit!  If you're diving into onchain app development, this is hands down the best library to kickstart your journey."
-        />
+        {secondHalf?.map((tweet) => (
+          <TweetCard key={tweet.username} {...tweet} />
+        ))}
       </Marquee>
     </div>
   );
