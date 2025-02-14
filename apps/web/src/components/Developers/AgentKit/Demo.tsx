46c09232ad96bf6708d7e40e922d34c85f641ef8
Alissa Crane <alissa.crane@coinbase.com>
2025-02-13 21:35:46 -0800

    fix border

--- apps/web/src/components/Developers/AgentKit/Demo.tsx
+++ apps/web/src/components/Developers/AgentKit/Demo.tsx
@@ -57,7 +57,7 @@ function Message({ text, content, type }: Item) {
   return (
     <figure
       className={cn(
-        'relative  flex min-h-fit w-full max-w-[500px] cursor-pointer overflow-hidden rounded-2xl',
+        'relative  flex min-h-fit w-full max-w-[500px] cursor-pointer overflow-hidden',
         'transition-all duration-200 ease-in-out',
         'bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]',
         'transform-gpu dark:bg-transparent dark:backdrop-blur-md',
