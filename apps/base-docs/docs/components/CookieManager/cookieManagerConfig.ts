a4c4c894a4266aa81ef6ab8f7e1d64f7ad1ca38c
Brendan Forster <brendan.forster@coinbase.com>
2025-02-19 12:47:44 -0800

    scratch updates

---
+++ apps/base-docs/docs/components/CookieManager/cookieManagerConfig.ts
@@ -0,0 +1,42 @@
+import { Framework, Region, TrackerType, TrackingCategory } from '@coinbase/cookie-manager';
+
+export const cookieManagerConfig = {
+  categories: [
+    {
+      id: TrackingCategory.NECESSARY,
+      expiry: 365,
+      required: true,
+      trackers: [
+        {
+          id: 'ip_country',
+          type: TrackerType.COOKIE,
+          expiry: 10,
+        },
+        {
+          id: 'locale',
+          type: TrackerType.COOKIE,
+        },
+      ],
+    },
+    {
+      id: TrackingCategory.PERFORMANCE,
+      expiry: 365,
+      trackers: [
+        {
+          id: 'base_device_id',
+          type: TrackerType.COOKIE,
+        },
+      ],
+    },
+  ],
+  geolocationRules: [
+    {
+      region: Region.DEFAULT,
+      framework: Framework.OPT_OUT,
+    },
+    {
+      region: Region.EU,
+      framework: Framework.OPT_IN,
+    },
+  ],
+};
