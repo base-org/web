351012495e515ea9f0d222136de6163fdd7c3e6a
Alissa Crane <alissa.crane@coinbase.com>
2025-02-19 12:15:40 -0800

    fix lint

--- apps/web/src/components/Builders/SmartWallet/Customers.tsx
+++ apps/web/src/components/Builders/SmartWallet/Customers.tsx
@@ -88,7 +88,7 @@ export function Customers() {
               <div className="flex w-[200px] items-center" key={`first-${customer.href}`}>
                 <Link href={customer.href} target="_blank">
                   <Image
-                    src={customer.logo as StaticImageData}
+                    src={customer.logo}
                     alt={String(customer.logo.src)}
                     style={logoStyle}
                     className="flex-none object-contain opacity-50 transition-opacity hover:opacity-100"
