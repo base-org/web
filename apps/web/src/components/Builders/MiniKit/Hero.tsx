011c8d15300fd3a755adba517fdb431d21d88b12
Brendan Forster <brendan.forster@coinbase.com>
2025-02-25 15:01:05 -0800

    update minikit cta

--- apps/web/src/components/Builders/MiniKit/Hero.tsx
+++ apps/web/src/components/Builders/MiniKit/Hero.tsx
@@ -52,7 +52,7 @@ export function Hero() {
             className="inline-flex items-center gap-2.5 rounded-lg bg-white px-4 pb-3 pt-3 font-medium text-dark-palette-primaryForeground transition-colors hover:bg-white/90 max-sm:mr-auto"
             onClick={handleCopy}
           >
-            npm create-onchain --mini
+            npm create onchain --mini
             {hasCopied ? (
               <div className="text-green-60">
                 <Icon name="checkmark" width="16" height="16" color="currentColor" />
