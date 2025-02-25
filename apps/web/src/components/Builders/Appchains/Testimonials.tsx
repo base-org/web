bcc97d764abcffed15a5e5618fde6d38a01f546d
Alissa Crane <alissacranespam@gmail.com>
2025-02-25 12:23:32 -0800

    add tweets

--- apps/web/src/components/Builders/Appchains/Testimonials.tsx
+++ apps/web/src/components/Builders/Appchains/Testimonials.tsx
@@ -1,8 +1,8 @@
 import { Marquee } from 'apps/web/src/components/Builders/Shared/Marquee';
 import { TweetCard } from 'apps/web/src/components/Builders/Shared/TweetCard';
-import tweet from 'apps/web/src/components/Builders/Appchains/tweet.png';
 import { TitleLevel } from 'apps/web/src/components/base-org/typography/Title/types';
 import Title from 'apps/web/src/components/base-org/typography/Title';
+import { TWEETS } from 'apps/web/src/components/Builders/Appchains/tweets';
 
 export function Testimonials() {
   return (
@@ -10,25 +10,17 @@ export function Testimonials() {
       <Title level={TitleLevel.Title1}>What our early customers are saying</Title>
       <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
         <Marquee className="[--duration:20s]" pauseOnHover>
-          <TweetCard
-            image={tweet}
-            name="sweetman.eth"
-            username="@sweetman.eth"
-            content="Just built a musician-friendly AI Agent using @base and @CoinbaseDev AgentKit! Watch how you can go from zero to shipping in less than 10 minutes—and maybe snag part of the $30k grants!"
-          />
-          <TweetCard
-            image={tweet}
-            name="sweetman.eth"
-            username="@sweetman.eth"
-            content="Just built a musician-friendly AI Agent using @base and @CoinbaseDev
-AgentKit! Watch how you can go from zero to shipping in less than 10 minutes—and maybe snag part of the $30k grants!"
-          />
-          <TweetCard
-            image={tweet}
-            name="sweetman.eth"
-            username="@sweetman.eth"
-            content="Just built a musician-friendly AI Agent using @base and @CoinbaseDev AgentKit! Watch how you can go from zero to shipping in less than 10 minutes—and maybe snag part of the $30k grants!"
-          />
+          {TWEETS?.map((tweet) => {
+            return (
+              <TweetCard
+                key={tweet.username}
+                image={tweet.image}
+                name={tweet.name}
+                username={tweet.username}
+                content={tweet.content}
+              />
+            );
+          })}
         </Marquee>
       </div>
     </div>
